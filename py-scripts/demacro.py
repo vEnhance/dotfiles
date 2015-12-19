dab9d2652ed72a209e717b752133a750f4155a90
Evan Chen <evanchen@mit.edu>
2015-12-19 14:36:57 -0800

    Fix path issue in demacro.py

--- py-scripts/demacro.py
+++ py-scripts/demacro.py
@@ -16,10 +16,10 @@ with open("%s.tex" %file_root_name) as f, open("/tmp/source.tex", "w") as g:
 			print >>g, r'\usepackage{demacro-private}'
 		print >>g, line.strip()
 
-os.system("cp -f /home/evan/Dropbox/Documents/Blog/demacro-private.sty /tmp")
+os.system("cp -f ~/dotfiles/py-scripts/demacro-private.sty /tmp")
 os.chdir("/tmp")
 os.system("de-macro source.tex")
-os.system("python2 /home/evan/Dropbox/Documents/Blog/latex2wp/latex2wp.py source-clean.tex")
+os.system("python2 ~/dotfiles/py-scripts/latex2wp.py source-clean.tex")
 os.chdir(current_dir)
 os.system("cp -f /tmp/source-clean.html %s.html" %file_root_name)
 os.system("cp -f /tmp/source-clean.tex %s.clean.tex" %file_root_name)
