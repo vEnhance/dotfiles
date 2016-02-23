9d9f337e3bd2ad935de4c060c694373252142380
Evan Chen <evanchen@mit.edu>
2016-02-23 15:54:00 -0500

    Improve demacro script
    Now commands in the preamble get processed too

--- py-scripts/demacro.py
+++ py-scripts/demacro.py
@@ -10,13 +10,32 @@ else:
 	file_root_name = arg
 	
 current_dir = os.getcwd()
+preamble = ""
+doc_started = False
 with open("%s.tex" %file_root_name) as f, open("/tmp/source.tex", "w") as g:
+	print >>g, f.readline().strip() # print \documentclass
 	for line in f:
 		if line.strip() == r'\begin{document}':
+			doc_started = True
 			print >>g, r'\usepackage{demacro-private}'
-		print >>g, line.strip()
+			print >>g, r'\def\blue{\bgroup\color{blue}}'
+			print >>g, r'\def\endblue{\egroup}'
+			print >>g, r'\def\red{\bgroup\color{red}}'
+			print >>g, r'\def\endred{\egroup}'
+			print >>g, r'\def\green{\bgroup\color{green}}'
+			print >>g, r'\def\endgreen{\egroup}'
+			print >>g, r''
+			print >>g, line.strip() # begin document
+		elif not doc_started:
+			preamble += line
+			if not r"newcommand" in line:
+				print >>g, line.strip()
+		elif doc_started:
+			print >>g, line.strip()
 
 os.system("cp -f ~/dotfiles/py-scripts/demacro-private.sty /tmp")
+with open("/tmp/demacro-private.sty", "a") as f:
+	print >>f, "\n" + preamble
 os.chdir("/tmp")
 os.system("de-macro source.tex")
 os.system("python2 ~/dotfiles/py-scripts/latex2wp.py source-clean.tex")
