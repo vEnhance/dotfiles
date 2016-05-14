7492b163a4eafbacd3ceacddfbeed00ac3edc4db
Evan Chen <evanchen@mit.edu>
2016-05-13 22:37:42 -0400

    demacro upgrades
    Copy \input'ed files as well
    Use a better /tmp directory

--- py-scripts/demacro.py
+++ py-scripts/demacro.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+TMP = "/tmp/demacro"
+os.system("mkdir -p %s" %TMP)
+
 arg = sys.argv[1]
 if ".tex" in arg:
 	file_root_name = arg[0:-4]
@@ -12,7 +15,7 @@ else:
 current_dir = os.getcwd()
 preamble = ""
 doc_started = False
-with open("%s.tex" %file_root_name) as f, open("/tmp/source.tex", "w") as g:
+with open("%s.tex" %file_root_name) as f, open("%s/source.tex" %TMP, "w") as g:
 	print >>g, f.readline().strip() # print \documentclass
 	for line in f:
 		if line.strip() == r'\begin{document}':
@@ -32,13 +35,13 @@ with open("%s.tex" %file_root_name) as f, open("/tmp/source.tex", "w") as g:
 				print >>g, line.strip()
 		elif doc_started:
 			print >>g, line.strip()
-
-os.system("cp -f ~/dotfiles/py-scripts/demacro-private.sty /tmp")
-with open("/tmp/demacro-private.sty", "a") as f:
+os.system("cp -f *.tex " + TMP)
+os.system("cp -f ~/dotfiles/py-scripts/demacro-private.sty " + TMP)
+with open("%s/demacro-private.sty" %TMP, "a") as f:
 	print >>f, "\n" + preamble
-os.chdir("/tmp")
+os.chdir(TMP)
 os.system("de-macro source.tex")
 os.system("python2 ~/dotfiles/py-scripts/latex2wp.py source-clean.tex")
 os.chdir(current_dir)
-os.system("cp -f /tmp/source-clean.html %s.html" %file_root_name)
-os.system("cp -f /tmp/source-clean.tex %s.clean" %file_root_name)
+os.system("cp -f %s/source-clean.html %s.html" %(TMP, file_root_name))
+os.system("cp -f %s/source-clean.tex %s.clean" %(TMP, file_root_name))
