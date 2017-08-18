ed47401184086fa02fa507a799ed18630a22fb09
Evan Chen <evanchen@mit.edu>
2017-08-18 16:17:51 -0700

    Only export termcap colors (not -U)

--- fish/config.fish
+++ fish/config.fish
@@ -156,12 +156,12 @@ function hunt ()
 	ls -l --color=tty
 end
 
-set -xU LESS_TERMCAP_md (printf "\e[01;31m")
-set -xU LESS_TERMCAP_me (printf "\e[0m")
-set -xU LESS_TERMCAP_se (printf "\e[0m")
-set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
-set -xU LESS_TERMCAP_ue (printf "\e[0m")
-set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+set -x LESS_TERMCAP_md (printf "\e[01;31m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[01;32m")
 
 #It speaks!
 alias hi="echo Hi!"
