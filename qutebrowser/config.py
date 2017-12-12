62a351126b79a4a9270a9cb1dbb84ae1f1f44533
Evan Chen <evanchen@mit.edu>
2017-12-12 13:26:56 -0500

    Change to config.py for qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,15 @@
+## qutebrowser config.py
+
+c.backend = 'webengine'
+c.content.pdfjs =  True
+c.statusbar.hide = False
+c.tabs.show = 'always'
+c.url.default_page = 'file:///home/evan/dotfiles/homepage.html'
+c.url.searchengines = {
+		'DEFAULT' : 'https://duckduckgo.com/?q={}'
+		}
+c.url.start_pages = 'file:///home/evan/dotfiles/homepage.html'
+# config.unbind('<Escape>', mode='insert')
+
+c.input.insert_mode.auto_load  = True
+c.input.insert_mode.auto_leave = False
