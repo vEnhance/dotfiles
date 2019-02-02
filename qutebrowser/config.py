16c7e0eb9f635ad97540fc255b3304552551ef73
Evan Chen <evanchen@mit.edu>
2019-02-02 11:57:07 -0500

    Move homepage onto evanchen.cc
    Works better that way I think
    http://web.evanchen.cc/static/browser-homepage.html

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -4,11 +4,11 @@ c.backend = 'webengine'
 c.content.pdfjs =  True
 c.statusbar.hide = False
 c.tabs.show = 'always'
-c.url.default_page = 'file:///home/evan/dotfiles/homepage.html'
+c.url.default_page = 'http://web.evanchen.cc/static/browser-homepage.html'
 c.url.searchengines = {
 		'DEFAULT' : 'https://duckduckgo.com/?q={}'
 		}
-c.url.start_pages = 'file:///home/evan/dotfiles/homepage.html'
+c.url.start_pages = c.url.default_page
 # config.unbind('<Escape>', mode='insert')
 
 c.input.insert_mode.auto_load  = True
