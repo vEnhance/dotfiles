b5b62a207d057790362c55d09673c486ee0d0de8
Evan Chen <evan@evanchen.cc>
2019-07-17 11:48:58 +0100

    Command to reset keyboard in i3

--- redo-keys.sh
+++ redo-keys.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-setxkbmap dvorak
-
-synclient TapButton1=0           # Disable tap to click
-synclient TapButton2=0           # Disable double tap to paste
-synclient RightButtonAreaRight=1 # Remap mouse buttons
 # Make sure we're in Dvorak
+setxkbmap dvorak -option caps:escape
+
+# synclient TapButton1=0           # Disable tap to click
+# synclient TapButton2=0           # Disable double tap to paste
+# synclient RightButtonAreaRight=1 # Remap mouse buttons
