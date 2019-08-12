0df736a005376fcf914895922924064fbce33eea
Evan Chen <evan@evanchen.cc>
2019-08-11 23:37:44 -0700

    scrot requires overwrite now

--- fuzzy-lock.sh
+++ fuzzy-lock.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # Take a screenshot
-scrot /tmp/screen_locked.png
+scrot --overwrite /tmp/screen_locked.png
 
 # mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 mogrify -blur 0x6 -brightness-contrast -20 /tmp/screen_locked.png
