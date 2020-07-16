150e5c5546960a3f610ef2e519ea6f279bb2ddf5
Evan Chen <evan@evanchen.cc>
2020-07-16 16:06:50 -0700

    Don't move loopback sinks to headphones
    Just application sinks will be fine, thank you...

--- sh-scripts/paswitch.sh
+++ sh-scripts/paswitch.sh
@@ -44,9 +44,9 @@ sink=${sink_names[$1]:-$1}
 (
 	echo set-default-sink $sink
 	
-	pacmd list-sink-inputs |
-		grep -E '^\s*index:' |
-		grep -oE '[0-9]+' |
+	pactl list sink-inputs short |
+		grep -v 'module-loopback.c' |
+		grep -oE '^[0-9]+' |
 		while read input
 	do
 		echo move-sink-input $input $sink
