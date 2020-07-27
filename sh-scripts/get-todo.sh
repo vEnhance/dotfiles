1f36b0089ce1d2939495962466a1e886db0e889a
Evan Chen <evan@evanchen.cc>
2020-07-27 14:15:41 -0700

    Integrate google calendar into conky-like

--- sh-scripts/get-todo.sh
+++ sh-scripts/get-todo.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
+touch /tmp/agenda.txt
 while :
 do
 	clear
-	echo "TODAY on the to-do list"
+	echo "~TODAY~ on Evan's to-do list"
 	date
-	echo "-------------"
+	echo "-------------------"
 	todoist sync
-	script --flush --quiet --return /tmp/todo.txt  --command "todoist --color list -f today" > /dev/null
+	script --flush --quiet --return /tmp/todo.txt  --command "todoist --color list -f overdue\|today" > /dev/null
 	cat /tmp/todo.txt | tail -n +2 | head -n -2 | cut -c 72- | sort -k 1.5
+	echo "-------------------"
+	cat /tmp/agenda.txt | cut -b 12-19,31-
+	gcalendar --no-of-days 1 --output txt \
+		--calendar "日曆" \
+		"Break" \
+		"Events" \
+		"Friends" \
+		"Garbage" \
+		"Happy Events" \
+		"Important" \
+		"Leisure" \
+		"Office Hours" \
+		"Prison" \
+		"Real Life" \
+		"Schedule" \
+		"Todoist" \
+		"Unfortunate Things" \
+		"Video Calls for OTIS" \
+		"Zero-Minute Reminders" \
+		"twitch.tv" > /tmp/agenda.txt &
 	echo ""
 	echo "Press ENTER to refresh..."
 	read -t 1800 &> /dev/null
