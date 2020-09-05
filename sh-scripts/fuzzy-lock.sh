209b2e283c36f8d644e7baf16ad14c5d170387d1
Evan Chen <evan@evanchen.cc>
2020-09-05 08:39:55 -0400

    Change i3lock script
    Different blurring mechanism
    Close workrave when screen locked, re-open upon return

--- sh-scripts/fuzzy-lock.sh
+++ sh-scripts/fuzzy-lock.sh
@@ -1,10 +1,11 @@
-#!/bin/sh -e
+#!/bin/bash
 
 if pgrep i3lock > /dev/null
 then
 	echo "Already running"
 	exit
 fi
+
 if pgrep stepmania > /dev/null
 then
 	echo "Stepmania running"
@@ -12,11 +13,26 @@ then
 	exit
 fi
 
+RESTART_WORKRAVE_AFTER=false
+if pgrep workrave > /dev/null
+then
+	killall workrave
+	RESTART_WORKRAVE_AFTER=true
+fi
+
+
 # Take a screenshot
 scrot --overwrite /tmp/screen_locked.png
 
 # mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-mogrify -blur 0x6 -brightness-contrast -20 /tmp/screen_locked.png
+mogrify -blur 3x4 +level 35% -brightness-contrast -30x0 /tmp/screen_locked.png
 
 # Lock screen displaying this image.
-i3lock -b -e -p win -i /tmp/screen_locked.png
+i3lock -f -b -e -p win -i /tmp/screen_locked.png
+# i3lock -f -b -e -p win -i /home/evan/Dropbox/Photos/Pictures/backgrounds/sans-papyrus.png -t
+
+
+if $RESTART_WORKRAVE_AFTER
+then
+	workrave &
+fi
