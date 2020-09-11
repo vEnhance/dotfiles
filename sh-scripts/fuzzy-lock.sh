72596cf31ec9533e5e6fe6f69eea73a1c89ff2ba
Evan Chen <evan@evanchen.cc>
2020-09-11 19:19:05 -0400

    Improved locking screen
    Dim screen with xset on lock
    Mute the microphone automatically

--- sh-scripts/fuzzy-lock.sh
+++ sh-scripts/fuzzy-lock.sh
@@ -13,13 +13,19 @@ then
 	exit
 fi
 
-RESTART_WORKRAVE_AFTER=false
+REVIVE_WORKRAVE=false;
 if pgrep workrave > /dev/null
 then
+	REVIVE_WORKRAVE=true;
 	killall workrave
-	RESTART_WORKRAVE_AFTER=true
 fi
 
+REVIVE_MIC=true;
+if ponymix -t source is-muted
+then
+	REVIVE_MIC=false;
+fi
+ponymix -t source mute
 
 # Take a screenshot
 scrot --overwrite /tmp/screen_locked.png
@@ -27,12 +33,33 @@ scrot --overwrite /tmp/screen_locked.png
 # mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 mogrify -blur 3x4 +level 35% -brightness-contrast -30x0 /tmp/screen_locked.png
 
+xset s 20 20
+
 # Lock screen displaying this image.
-i3lock -f -b -e -p win -i /tmp/screen_locked.png
-# i3lock -f -b -e -p win -i /home/evan/Dropbox/Photos/Pictures/backgrounds/sans-papyrus.png -t
+i3lock \
+	--beep \
+	--ignore-empty-password \
+	--show-failed-attempts \
+	--nofork \
+	--pointer=win \
+	--image=/tmp/screen_locked.png
 
+# i3lock -n -f -b -e -p win -i /home/evan/Dropbox/Photos/Pictures/backgrounds/sans-papyrus.png -t
 
-if $RESTART_WORKRAVE_AFTER
+xset s 600 600
+
+if $REVIVE_WORKRAVE;
 then
+	sleep 1
 	workrave &
+	dbus-send --print-reply \
+		--dest=org.workrave.WorkraveApplication \
+		/org/workrave/Workrave/Core \
+		org.workrave.CoreInterface.SetOperationMode \
+		string:'normal'
+fi
+
+if $REVIVE_MIC;
+then
+	ponymix -t source unmute
 fi
