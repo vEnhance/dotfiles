ba101d0a67901b64bab75b54949620790aaa9ce2
Evan Chen <evan@evanchen.cc>
2020-12-18 17:40:15 -0500

    Switch to xss-lock and dex

---
+++ sh-scripts/startup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Supports desktop files, so handled by dex:
+# * xfce power manager
+# * nm applet
+# * redshift gtk
+# * dropbox
+
+if [ "$HOSTNAME" = ArchAngel ]; then
+	picom -C -G -b --no-fading-openclose
+fi
+
+if [ "$HOSTNAME" = ArchScythe ]; then
+	picom -C -G -b --no-fading-openclose
+	# cbatticon -u 300 &
+fi
+
+if [ "$HOSTNAME" = ArchMajestic ]; then
+	picom -b &
+	clipmenud &
+	if [ "$(whoami)" = "evan" ]; then
+		xfce4-terminal -e "/home/evan/dotfiles/sh-scripts/get-todo.sh" &
+		qtalarm &
+		# ibus-daemon -d -r &
+	fi
+fi
+
+xss-lock -n ~/dotfiles/sh-scripts/lock-warning.sh -- ~/dotfiles/sh-scripts/fuzzy-lock.sh &
