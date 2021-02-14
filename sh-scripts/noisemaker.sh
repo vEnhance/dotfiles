1f3c346e1146fb2b1a3afe5e23bf7dd4dbb35105
Evan Chen <evan@evanchen.cc>
2021-02-14 10:57:01 -0500

    Noisemaker use relative path

--- sh-scripts/noisemaker.sh
+++ sh-scripts/noisemaker.sh
@@ -3,7 +3,7 @@
 if pgrep -U $(whoami) mpg123 > /dev/null
 then
 	if [ "$1" = a ]; then
-		mpg123 -f -18000 /home/star/dotfiles/noisemaker/Sa-horn.mp3
+		mpg123 -f -18000 $HOME/dotfiles/noisemaker/Sa-horn.mp3
 		exit
 	else
 		pkill mpg123
@@ -11,43 +11,43 @@ then
 fi
 
 if [ "$1" = 1 ]; then
-	mpg123 /home/star/dotfiles/noisemaker/S1-applause.mp3
+	mpg123 $HOME/dotfiles/noisemaker/S1-applause.mp3
 fi
 
 if [ "$1" = 2 ]; then
-	mpg123 -f -9000 /home/star/dotfiles/noisemaker/S2-success.mp3
+	mpg123 -f -9000 $HOME/dotfiles/noisemaker/S2-success.mp3
 fi
 
 if [ "$1" = 3 ]; then
-	mpg123 -f 15000 /home/star/dotfiles/noisemaker/S3-fanfare.mp3
+	mpg123 -f 15000 $HOME/dotfiles/noisemaker/S3-fanfare.mp3
 fi
 
 if [ "$1" = 4 ]; then
-	mpg123 /home/star/dotfiles/noisemaker/S4-correct.mp3
+	mpg123 $HOME/dotfiles/noisemaker/S4-correct.mp3
 fi
 
 if [ "$1" = 5 ]; then
-	mpg123 /home/star/dotfiles/noisemaker/S5-cashreg.mp3
+	mpg123 $HOME/dotfiles/noisemaker/S5-cashreg.mp3
 fi
 
 if [ "$1" = 6 ]; then
-	mpg123 -f -13000 /home/star/dotfiles/noisemaker/S6-metal.mp3
+	mpg123 -f -13000 $HOME/dotfiles/noisemaker/S6-metal.mp3
 fi
 
 if [ "$1" = 7 ]; then
-	mpg123 /home/star/dotfiles/noisemaker/S7-drum.mp3
+	mpg123 $HOME/dotfiles/noisemaker/S7-drum.mp3
 fi
 
 if [ "$1" = 8 ]; then
-	mpg123 /home/star/dotfiles/noisemaker/S8-boo.mp3
+	mpg123 $HOME/dotfiles/noisemaker/S8-boo.mp3
 fi
 
 if [ "$1" = 9 ]; then
-	mpg123 -f -18000 /home/star/dotfiles/noisemaker/S9-taratata.mp3
+	mpg123 -f -18000 $HOME/dotfiles/noisemaker/S9-taratata.mp3
 fi
 
 if [ "$1" = a ]; then
-	mpg123 -f -18000 /home/star/dotfiles/noisemaker/Sa-horn.mp3
+	mpg123 -f -18000 $HOME/dotfiles/noisemaker/Sa-horn.mp3
 fi
 
 
