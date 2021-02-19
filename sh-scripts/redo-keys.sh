f390f63be1e743ef73b08068a68a98070dbd9a80
Evan Chen <evan@evanchen.cc>
2021-02-19 09:41:44 -0500

    Try to always enable num lock

--- sh-scripts/redo-keys.sh
+++ sh-scripts/redo-keys.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
 # Make sure we're in Dvorak
+xmodmap -e "remove lock = Caps_Lock"
 setxkbmap dvorak -option caps:escape
+numlockx on
 
 # synclient TapButton1=0           # Disable tap to click
 # synclient TapButton2=0           # Disable double tap to paste
