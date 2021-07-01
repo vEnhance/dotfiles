48b6068d5436254831b2abda79de7fc3961dda9f
Evan Chen <evan@evanchen.cc>
2021-07-01 14:59:59 -0500

    Escape properly and other polish

--- py-scripts/xfer.py
+++ py-scripts/xfer.py
@@ -2,6 +2,7 @@ import argparse
 import getpass
 from hashlib import sha512
 from subprocess import call
+from pathlib import Path
 
 def h(text: str) -> str:
 	return sha512(text.encode('UTF-8')).hexdigest()
@@ -12,18 +13,25 @@ parser.add_argument('filename',
 		help = 'Path of file to upload')
 parser.add_argument('-n', '--name',
 		help = 'Name of the file to upload.')
+parser.add_argument('-p', '--password',
+		help = 'Path to a password file, otherwise read from getpass.')
+parser.add_argument('-d', '--dry-run', action='store_true',
+		help = 'Dry run, do not actually upload file.')
 args = parser.parse_args()
 
 if __name__ == "__main__":
-	password = getpass.getpass().strip()
+	if args.password:
+		password = Path(args.password).read_text().strip()
+	else:
+		password = getpass.getpass().strip()
 	filename = args.name or args.filename
 	kludge = 'evanchen.cc/xfer|' + filename + '|' + password
 	h1 = h(kludge)
 	h2 = h(h1)
 	checksum = h2[0:6]
-	call(f'gsutil cp {args.filename} '
-			f'gs://web.evanchen.cc/xfer-payload/{h1} '
-			f'> /dev/null',
-			shell=True)
+	if not args.dry_run:
+		call(f'gsutil cp "{args.filename}" '
+				f'gs://web.evanchen.cc/xfer-payload/{h1} '
+				f'> /dev/null',
+				shell=True)
 	print(f'https://web.evanchen.cc/xfer.html?f={filename}&h={checksum}')
-
