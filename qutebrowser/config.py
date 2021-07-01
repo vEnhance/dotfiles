67265ace9b61ab651402ed920c8ea51e36a0e4a4
Evan Chen <evan@evanchen.cc>
2021-07-01 14:53:21 -0500

    qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -4,9 +4,9 @@ config.load_autoconfig()
 
 c.backend = 'webengine'
 c.content.pdfjs =  True
-c.statusbar.hide = False
+# c.statusbar.hide = False
 c.tabs.show = 'always'
-c.url.default_page = 'http://web.evanchen.cc/static/browser-homepage.html'
+c.url.default_page = 'file:///home/evan/Documents/www/static/browser-homepage.html'
 c.url.searchengines = {
 		'DEFAULT' : 'https://duckduckgo.com/?q={}'
 		}
@@ -15,3 +15,7 @@ c.url.start_pages = c.url.default_page
 
 c.input.insert_mode.auto_load  = True
 c.input.insert_mode.auto_leave = False
+c.content.javascript.enabled = False
+
+config.bind('gt', 'tab-next')
+config.bind('x', 'tab-close')
