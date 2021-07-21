b25fc76060bcb7a6fd7f9ff7d2dc09e1d0ca1cd8
Evan Chen <evan@evanchen.cc>
2021-07-21 12:22:28 -0700

    Experiment with TaskWarrior

---
+++ sh-scripts/tw-rofi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+default="--------------------"
+lines="${default}\n$(task next rc.verbose=nothing)"
+chosen=$(echo -e "$lines" | rofi -dmenu)
+
+if [ -z "$chosen" ]; then
+	echo "Empty"
+elif [ "$chosen" = "$default" ]; then
+	echo "Default"
+elif [[ "$lines" = *"$chosen"* ]]; then
+	taskid=$(echo $chosen | awk '{print $1}')
+	echo $taskid
+	task $taskid done
+else
+	task add $chosen
+fi
