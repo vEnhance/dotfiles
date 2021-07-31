ad7154c496f272695adcc3339ae5044a63e4eb0c
Evan Chen <evan@evanchen.cc>
2021-07-31 07:55:58 -0700

    Integrate in ctwenty with i3bar

--- py-scripts/ctwenty.py
+++ py-scripts/ctwenty.py
@@ -1,13 +1,18 @@
 #!/bin/python3
 
+from datetime import datetime, timedelta
 import signal
 import psutil
 from subprocess import Popen
+from typing import Optional
+from pathlib import Path
 
 def cmd(s: str):
 	return Popen(s, shell=True).wait()
 def audio_block():
 	cmd(r'touch ~/.cache/ctwenty.lock')
+	# TODO: I don't think this dpms thing is that elegant
+	# it'd be better to spawn a giant fullscreen window that blocks everything
 	cmd(r'xset dpms force off')
 	cmd(r'mpg123 ~/dotfiles/noisemaker/435923_luhenriking.mp3')
 	cmd(r'xset dpms force on')
@@ -18,16 +23,34 @@ def audio_block():
 	cmd(r'i3-msg fullscreen toggle')
 	cmd(r'rm -f ~/.cache/ctwenty.lock')
 
+
+def write_next_time(current_status : int, seconds: Optional[int] = None):
+	p = Path('~/.cache/ctwenty.target').expanduser()
+	if current_status == -1:
+		if p.exists():
+			p.unlink()
+	else:
+		assert seconds is not None
+		t = datetime.now() + timedelta(seconds = seconds)
+		if current_status == 2:
+			emoji = "💛"
+		elif current_status == 1:
+			emoji = "💙"
+		else:
+			emoji = "🤍"
+		p.write_text(f"{emoji}{t.strftime('%H:%M')}")
+
 # we need to make sure the signals don't end the program
 SIGNALS = [signal.SIGALRM, signal.SIGUSR1, signal.SIGTSTP,
 		signal.SIGINT, signal.SIGTERM, signal.SIGCONT]
 def noop(signum, frame):
-	pass
+	print(signum, frame)
 for s in SIGNALS:
 	signal.signal(s, noop)
 
 current_status = -1
 signal.alarm(120)
+write_next_time(-1)
 # -1: manually disabled until next time i3 unlocks
 # 0: neutral state
 # 1: first warning
@@ -54,16 +77,20 @@ while True:
 
 	if current_status == -1:
 		t = 0 # wait until next i3lock
+		write_next_time(-1)
 	elif current_status == 0:
 		t = 1000
+		write_next_time(0, 1337)
 	elif current_status == 1:
 		cmd(r'notify-send -i timer-symbolic -t 5000 "Rest your eyes!" '\
-				r'"You have a mandatory break coming up soon in 4 minutes"')
+				r'"You have a mandatory break coming up soon in 6 minutes"')
 		t = 300
+		write_next_time(1, 300)
 	elif current_status == 2:
 		cmd(r'notify-send -i timer-symbolic -t 5000 "Rest your eyes!" '\
-				r'"You have a mandatory break coming up soon in 4 minutes"')
-		t = 60
+				r'"You have a mandatory break coming up soon in 37 seconds"')
+		t = 37
+		write_next_time(2, 37)
 	else:
 		raise ValueError("wtf?")
 
