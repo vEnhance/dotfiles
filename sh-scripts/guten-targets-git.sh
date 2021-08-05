eafc06a227777863b93900ccdea5e1fb5118db7f
Evan Chen <evan@evanchen.cc>
2021-08-05 13:40:09 -0700

    oops quotes

--- sh-scripts/guten-targets-git.sh
+++ sh-scripts/guten-targets-git.sh
@@ -4,7 +4,7 @@ git ls-files
 
 mkdir -p .git/gutentags/
 
-if [ -n $VIRTUAL_ENV ]; then
+if [ -n "$VIRTUAL_ENV" ]; then
 	ln -sf $VIRTUAL_ENV .git/gutentags/virtual
 	find .git/gutentags/virtual/ -name \*.py
 	ln -sf /usr/lib/python3.9/ .git/gutentags/system
