f7ed4f5072695a63593823e8309da1d41a54229c
Evan Chen <evan@evanchen.cc>
2021-08-05 21:38:53 -0700

    For now only do tags for python virtualenvs

--- sh-scripts/guten-targets-git.sh
+++ sh-scripts/guten-targets-git.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-git ls-files
-
-mkdir -p .git/gutentags/
-
+# virtualenv
 if [ -n "$VIRTUAL_ENV" ]; then
+	git ls-files
+	mkdir -p .git/gutentags/
 	ln -sf $VIRTUAL_ENV .git/gutentags/virtual
 	find .git/gutentags/virtual/ -name \*.py
 	ln -sf /usr/lib/python3.9/ .git/gutentags/system
