b1f2456314836b39323a8dee676b6b5bcc74b962
Evan Chen <evan@evanchen.cc>
2021-08-07 16:47:54 -0700

    Pretty printing i guess

--- venueQ/venueQ.py
+++ venueQ/venueQ.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from pprint import pformat
 from typing import Any, Dict, List, Optional
 
 VENUE_NAME_FIELD = 'venue-name'
@@ -7,9 +8,11 @@ VENUE_CHILDREN_FIELD = 'venue-children'
 class VenueNode:
 	name : str = '' # name must be unique
 	parent : 'VenueNode'
-	children: List['VenueNode'] = []
+	children : List['VenueNode']
+	root_path : Path = Path('.')
 
-	def __init__(self, data : Dict, parent : Optional['VenueNode']):
+	def __init__(self, data : Dict, parent : Optional['VenueNode'] = None):
+		self.children = []
 		if parent is not None:
 			self.parent = parent
 		else:
@@ -19,9 +22,9 @@ class VenueNode:
 		if children_dicts is not None:
 			child_dict : Dict[str, Any]
 			for child_dict in children_dicts:
-				cls = self.get_class_name(child_dict)
+				cls = self.get_class(child_dict)
 				self.children.append(
-					vars()[cls](data = child_dict, parent = self,)
+					cls(data = child_dict, parent = self,)
 					)
 		self.data = data
 	@property
@@ -31,15 +34,27 @@ class VenueNode:
 	def is_directory(self) -> bool:
 		return len(self.children) > 0
 
-	def get_class_name(self, child_dict : Dict[str, Any]):
-		return 'VenueNode'
+	def get_class(self, child_dict : Dict[str, Any]) -> type:
+		return type(self)
 	def get_extension(self) -> str:
 		return '.yaml'
 	def get_parent_path(self) -> Path:
 		if self.is_root:
-			return Path('.')
+			return self.root_path
 		else:
 			return self.parent.get_parent_path() / self.parent.name
 	def get_path(self) -> Path:
 		return self.get_parent_path() \
 				/ f'{self.name}.{self.get_extension()}'
+	
+	@property
+	def debug_dict(self) -> Dict[str, Any]:
+		d : Dict[str, Any] = self.data
+		d["CLASS"] = type(self)
+		d["NAME"] = self.name
+		if self.is_directory:
+			d["NODES"] = [c.debug_dict for c in self.children]
+		return d
+
+	def __str__(self) -> str:
+		return pformat(self.debug_dict)
