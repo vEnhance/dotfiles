e1129c72606b190349a0068572cd6322f2f00069
Evan Chen <evan@evanchen.cc>
2021-08-16 15:18:21 -0700

    More options for xfer script

--- py-scripts/xfer.py
+++ py-scripts/xfer.py
@@ -1,8 +1,9 @@
 import argparse
 import getpass
 from hashlib import sha512
-from subprocess import call
 from pathlib import Path
+from subprocess import call
+
 
 def h(text: str) -> str:
 	return sha512(text.encode('UTF-8')).hexdigest()
@@ -15,6 +16,10 @@ parser.add_argument('-n', '--name',
 		help = 'Name of the file to upload.')
 parser.add_argument('-p', '--password',
 		help = 'Path to a password file, otherwise read from getpass.')
+parser.add_argument('-i', '--insecure',
+		help = 'Specify the password via command line (insecure)')
+parser.add_argument('-e', '--echo', action = 'store_true',
+		help = "Don't hide the password with getpass.")
 parser.add_argument('-d', '--dry-run', action='store_true',
 		help = 'Dry run, do not actually upload file.')
 args = parser.parse_args()
@@ -22,8 +27,14 @@ args = parser.parse_args()
 if __name__ == "__main__":
 	if args.password:
 		password = Path(args.password).read_text().strip()
+	elif args.insecure:
+		password = args.insecure
+	elif args.echo:
+		password = input('Password: ').strip()
 	else:
 		password = getpass.getpass().strip()
+		password_confirm = getpass.getpass(prompt = 'Repeat: ').strip()
+		assert password == password_confirm, "nope, try again noob"
 	filename = args.name or args.filename
 	kludge = 'evanchen.cc/xfer|' + filename + '|' + password
 	h1 = h(kludge)
