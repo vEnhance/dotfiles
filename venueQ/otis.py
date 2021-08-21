aa97b394662f0f6658d30dbcf97ff7ee5d5ce3d4
Evan Chen <evan@evanchen.cc>
2021-08-20 21:25:07 -0700

    Massive cleanup

--- venueQ/otis.py
+++ venueQ/otis.py
@@ -16,29 +16,33 @@ OTIS_PDF_PATH = Path('/tmp/otis-pdf')
 if not OTIS_PDF_PATH.exists():
 	OTIS_PDF_PATH.mkdir()
 
+
 def send_email(subject: str, recipient: str, body: str):
 	mail = MIMEMultipart('alternative')
 	mail['From'] = 'OTIS Overlord <evan@evanchen.cc>'
 	mail['To'] = recipient
 	mail['Subject'] = subject
-	subprocess.run(['python',
+	subprocess.run(
+		['python',
 			Path('~/dotfiles/mutt/mutt-markdown.py').expanduser().absolute().as_posix()],
-		input = body,
-		text = True,
-		)
+		input=body,
+		text=True,
+	)
 	html_msg = Path('/tmp/neomutt-alternative.html').read_text()
 	mail.attach(MIMEText(body, 'plain'))
 	mail.attach(MIMEText(html_msg, 'html'))
 
 	password = subprocess.run(
-			['secret-tool', 'lookup', 'user', 'evanchen.mit', 'type', 'gmail'],
-			text = True, capture_output = True
-			).stdout
+		['secret-tool', 'lookup', 'user', 'evanchen.mit', 'type', 'gmail'],
+		text=True,
+		capture_output=True
+	).stdout
 
 	session = smtplib.SMTP('smtp.gmail.com', 587)
-	session.starttls(context = ssl.create_default_context())
+	session.starttls(context=ssl.create_default_context())
 	session.login('evanchen.mit@gmail.com', password)
 	session.sendmail('evan@evanchen.cc', recipient, mail.as_string())
+
 
 load_dotenv(Path('~/SkyNet/private/.env').expanduser())
 TOKEN = os.getenv('OTIS_WEB_TOKEN')
@@ -49,93 +53,103 @@ if PRODUCTION:
 else:
 	DASHBOARD_API_URL = 'http://127.0.0.1:8000/dash/api/'
 
+
 def query_otis_server(payload: Data) -> bool:
 	payload['token'] = TOKEN
-	resp = requests.post(DASHBOARD_API_URL, data = payload)
+	resp = requests.post(DASHBOARD_API_URL, data=payload)
 	if resp.status_code == 200:
 		logger.info("Got a 200 response back from server")
-		subprocess.run([
-			Path('~/dotfiles/sh-scripts/noisemaker.sh').expanduser().absolute().as_posix(),
-			'5'
-			])
+		subprocess.run(
+			[Path('~/dotfiles/sh-scripts/noisemaker.sh').expanduser().absolute().as_posix(), '5']
+		)
 		return True
 	else:
 		logger.error(f"OTIS-WEB threw an exception with status code {resp.status_code}\n" \
-				+ resp.content.decode('utf-8'))
+        + resp.content.decode('utf-8'))
 		return False
 
 
 class ProblemSet(VenueQNode):
 	def get_initial_data(self) -> Data:
 		return {
-				'action' : 'grade_problem_set',
-				}
+			'action': 'grade_problem_set',
+		}
+
 	def get_name(self, data: Data) -> str:
 		return str(data['pk'])
+
 	@property
 	def pdf_target_path(self):
 		def clean(key) -> str:
 			return ''.join(c for c in self.data[key] if c in string.ascii_letters)
 		fname = \
-				clean('student__user__first_name') \
-				+ clean('student__user__last_name') \
-				+ '-' \
-				+ clean('unit__code') \
-				+ '-' \
-				+ clean('unit__group__name')
+        clean('student__user__first_name') \
+        + clean('student__user__last_name') \
+        + '-' \
+        + clean('unit__code') \
+        + '-' \
+        + clean('unit__group__name')
 		return OTIS_PDF_PATH / f"otis_{fname}.pdf"
+
 	def init_hook(self):
 		self.data['approved'] = True
 		if not self.pdf_target_path.exists():
 			url = f"https://storage.googleapis.com/otisweb-media/{self.data['upload__content']}"
 			pdf_response = requests.get(url=url)
 			self.pdf_target_path.write_bytes(pdf_response.content)
+
 	def on_buffer_open(self, data: Data):
 		super().on_buffer_open(data)
-		self.edit_temp(extension = 'mkd')
+		self.edit_temp(extension='mkd')
 		subprocess.Popen(['zathura', self.pdf_target_path.absolute()])
+
 	def on_buffer_close(self, data: Data):
 		super().on_buffer_close(data)
-		if data['approved']:
-			comments_to_email = self.read_temp(extension = 'mkd').strip()
+		comments_to_email = self.read_temp(extension='mkd').strip()
+		if data['approved'] and comments_to_email != '':
 			if data.get('next_unit_to_unlock__pk', None):
 				comments_to_email += '\n\n'
 				comments_to_email += \
-						f"I unlocked {data['next_unit_to_unlock__code']} {data['next_unit_to_unlock__group__name']}."
+              f"I unlocked {data['next_unit_to_unlock__code']} {data['next_unit_to_unlock__group__name']}."
 			recipient = data['student__user__email']
 			subject = f"OTIS: {data['unit__code']} {data['unit__group__name']} checked off"
 			try:
-				send_email(subject = subject,
-						recipient = recipient,
-						body = comments_to_email)
+				send_email(subject=subject, recipient=recipient, body=comments_to_email)
 			except Exception as e:
 				logger.error(f"Email {subject} to {recipient} failed", exc_info=e)
 			else:
 				logger.info(f"Email {subject} to {recipient} sent!")
-			if query_otis_server(payload = data) is True:
+			if query_otis_server(payload=data) is True:
 				self.delete()
+
 
 class ProblemSetCarrier(VenueQNode):
 	def get_class_for_child(self, _: Data):
 		return ProblemSet
 
+
 class Inquiries(VenueQNode):
 	def init_hook(self):
 		self.data['approve_all'] = False
+
 	def on_buffer_close(self, data: Data):
 		super().on_buffer_close(data)
 		if data['approve_all']:
-			if query_otis_server(payload = {'action' : 'approve_inquiries'}):
+			if query_otis_server(payload={'action': 'approve_inquiries'}):
 				self.delete()
+
 
 class Suggestion(VenueQNode):
 	def get_name(self, data: Data) -> str:
 		return str(data['pk'])
+
 	pass
+
 
 class SuggestionCarrier(VenueQNode):
 	def get_class_for_child(self, _: Data):
 		return Suggestion
+
 
 class OTISRoot(VenueQRoot):
 	def get_class_for_child(self, child_dict: Data):
@@ -148,13 +162,11 @@ class OTISRoot(VenueQRoot):
 		else:
 			raise ValueError('wtf is ' + child_dict['_name'])
 
+
 if __name__ == "__main__":
-	otis_response = requests.post(
-			url = DASHBOARD_API_URL,
-			data = {'token' : TOKEN, 'action' : 'init'}
-			)
+	otis_response = requests.post(url=DASHBOARD_API_URL, data={'token': TOKEN, 'action': 'init'})
 
 	otis_dir = Path('/tmp/otis') if PRODUCTION else Path('/tmp/otis-debug')
 	if not otis_dir.exists():
 		otis_dir.mkdir()
-	ROOT_NODE = OTISRoot(otis_response.json(), root_dir = otis_dir)
+	ROOT_NODE = OTISRoot(otis_response.json(), root_dir=otis_dir)
--- venueQ/venueQ.py
+++ venueQ/venueQ.py
@@ -10,7 +10,7 @@ try:
 	import vim  # type: ignore
 
 except ImportError:
-	vim: Any = None # squelch type errors, ty pyright
+	vim: Any = None  # squelch type errors, ty pyright
 	VIM_ENABLED = False
 else:
 	VIM_ENABLED = True
@@ -23,24 +23,29 @@ logger = logging.getLogger('venueQ')
 logger.setLevel(logging.DEBUG)
 
 if VIM_ENABLED:
-	formatter = logging.Formatter('[{levelname}] {asctime} {module} {name}: {message}\n',
-			style = '{')
+	formatter = logging.Formatter(
+		'[{levelname}] {asctime} {module} {name}: {message}\n', style='{'
+	)
 	for b in vim.buffers:
 		if "venueQlog" in b.name:
 			VIM_LOG_BUFFER = b
 			break
 	else:
 		raise Exception("Couldn't find the venueQ log buffer")
+
 	class VimLogHandler(logging.Handler):
 		level = 0
+
 		def emit(self, record: logging.LogRecord):
 			msg = formatter.format(record)
 			for line in msg.splitlines():
 				VIM_LOG_BUFFER.append(line)
+
 	logger.addHandler(VimLogHandler())
 
+
 class VenueQNode:
-	name: str = '' # name must be unique
+	name: str = ''  # name must be unique
 	parent: 'VenueQNode'
 	root: 'VenueQRoot'
 	is_directory = False
@@ -58,6 +63,7 @@ class VenueQNode:
 		self.update_by_dictionary(data)
 		self.init_hook()
 		self.save()
+
 	def update_by_dictionary(self, data: Data):
 		children_dicts = data.pop(VENUE_CHILDREN_FIELD, None)
 		if children_dicts is not None:
@@ -67,56 +73,70 @@ class VenueQNode:
 				# this is pretty expensive to recreate the object
 				# only to see if it exists already
 				cls = self.get_class_for_child(child_dict)
-				node = cls(data = child_dict, parent = self)
+				node = cls(data=child_dict, parent=self)
 				if node.pk not in self.root.lookup:
 					self.root.lookup[node.pk] = node
 				else:
 					self.root.lookup[node.pk].update_by_dictionary(child_dict)
 		self.data.update(data)
 		self.process_data()
+
 	def get_initial_data(self) -> Data:
 		if self.path.exists():
 			return self.load()
 		else:
 			return self.get_default_data()
+
 	def temp_path(self, extension: str, name: str = None):
 		return self.directory / f'{name or self.name}.tmp.{extension}'
+
 	def edit_temp(self, extension: str, name: str = None):
 		if VIM_ENABLED:
 			vim.command(f":split {self.temp_path(extension, name)}")
 		else:
-			subprocess.run(['vim', self.temp_path(extension, name)], shell = True)
+			subprocess.run(['vim', self.temp_path(extension, name)], shell=True)
+
 	def read_temp(self, extension: str, name: str = None):
-		text = self.temp_path(extension, name).read_text()
-		self.root.queue_wipe(self.temp_path(extension, name))
-		return text
+		if self.temp_path(extension, name).exists():
+			text = self.temp_path(extension, name).read_text()
+			self.root.queue_wipe(self.temp_path(extension, name))
+			return text
+		else:
+			return ''
 
 	@property
 	def pk(self) -> str:
 		return self.path.absolute().as_posix()
+
 	@property
 	def directory(self) -> Path:
 		if self.is_root:
 			return self.root.root_dir
 		else:
 			return self.parent.directory / self.parent.name
+
 	@property
 	def path(self) -> Path:
 		return self.directory \
-				/ f'{self.name}.{self.get_extension()}'
+        / f'{self.name}.{self.get_extension()}'
+
 	def __eq__(self, other) -> bool:
 		return self.pk == other.pk
+
 	def delete(self):
 		self.root.queue_wipe(self.path)
 		del self.root.lookup[self.pk]
+
 	def mkdir(self):
 		if not self.parent.directory.exists():
 			self.parent.mkdir()
 		if not self.directory.exists():
 			self.directory.mkdir()
+
 	def save(self):
 		self.mkdir()
 		self.path.write_text(self.dump())
+
 	def read(self):
 		return self.path.read_text()
 
@@ -138,12 +158,15 @@ class VenueQNode:
 
 	def get_default_data(self) -> Data:
 		return {}
+
 	def init_hook(self):
 		"""Hook called just before saving data each time the node is initialized"""
 		pass
+
 	def process_data(self):
 		""""Post update hook called each time this node has its dictionary updated"""
 		pass
+
 	def get_extension(self) -> str:
 		"""Returns the file extension for these venueQ nodes"""
 		return 'venueQ.yaml'
@@ -164,7 +187,7 @@ class VenueQNode:
 	def dump(self):
 		"""This method serializes the dictionary object to save to disk.
 		Override it to change how the data on disk is interpreted."""
-		return yaml.dump(self.data, indent = True, default_flow_style=False)
+		return yaml.dump(self.data, indent=True, default_flow_style=False)
 
 	def on_buffer_open(self, data: Data):
 		"""This method is called when the buffer is loaded.
@@ -177,6 +200,7 @@ class VenueQNode:
 		This is called with an argument data = self.load().
 		Override this to perform actions."""
 		self.data.update(data)
+
 
 class VenueQRoot(VenueQNode):
 	is_root = True
@@ -200,8 +224,9 @@ class VenueQRoot(VenueQNode):
 					break
 			else:
 				logger.warn(f"Tried to wipe {p} but found no buffer for it among " \
-						+ ', '.join(b.name for b in vim.buffers))
+              + ', '.join(b.name for b in vim.buffers))
 		p.unlink()
+
 	def wipe(self):
 		if VIM_ENABLED:
 			for bn in self.wipe_queue:
