517b6821289c079dfb42a2ed62fdec6da29db5f6
Evan Chen <evan@evanchen.cc>
2021-09-02 10:35:55 -0700

    feat(gray): exempt *.github.com

--- gm-scripts/GrayscaleInternet.user.js
+++ gm-scripts/GrayscaleInternet.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Grayscale Internet
 // @namespace    https://web.evanchen.cc/
-// @version      1.1.8
+// @version      1.1.9
 // @description  Turn things to black and white
 // @author       Evan Chen
 // @match        http://*/*
 // @match        https://*/*
 // @exclude      https://github.com/*
+// @exclude      https://*.github.com/*
 // @exclude      https://*.djangoproject.com/*
 // @exclude      https://calendar.google.com/*
 // @exclude      https://drive.google.com/*
@@ -46,24 +47,30 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // ==/UserScript==
 
-(function() {
-  'use strict';
-  const head = document.getElementsByTagName('head')[0];
-  const body = document.getElementsByTagName('body')[0];
+(function () {
+	"use strict";
+	const head = document.getElementsByTagName("head")[0];
+	const body = document.getElementsByTagName("body")[0];
 
-  const oldbg = window.getComputedStyle(body, null).getPropertyValue("background-color");
-  function getRGB(str){
-    var match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
-    return match ? {
-      red: Number(match[1]),
-      green: Number(match[2]),
-      blue: Number(match[3])
-    } : {};
-  }
+	const oldbg = window
+		.getComputedStyle(body, null)
+		.getPropertyValue("background-color");
+	function getRGB(str) {
+		var match = str.match(
+			/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/
+		);
+		return match
+			? {
+					red: Number(match[1]),
+					green: Number(match[2]),
+					blue: Number(match[3]),
+			  }
+			: {};
+	}
 
-  const rgb = getRGB(oldbg);
-  const avg = Math.round((rgb.red + rgb.green + rgb.blue) / 3) || 255;
-  const elm = document.createElement('style');
-  elm.innerHTML = `html { filter: grayscale(100%) brightness(80%) contrast(1.5); background-color: rgb(${avg}, ${avg}, ${avg}) }`;
-  head.appendChild(elm);
+	const rgb = getRGB(oldbg);
+	const avg = Math.round((rgb.red + rgb.green + rgb.blue) / 3) || 255;
+	const elm = document.createElement("style");
+	elm.innerHTML = `html { filter: grayscale(100%) brightness(80%) contrast(1.5); background-color: rgb(${avg}, ${avg}, ${avg}) }`;
+	head.appendChild(elm);
 })();
