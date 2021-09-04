270279e0bce429027a5ba4b43a579e09c5a5411e
Evan Chen <evan@evanchen.cc>
2021-09-04 17:34:11 -0400

    style(c20): lint, accept i3lock-color too

--- py-scripts/ctwenty.py
+++ py-scripts/ctwenty.py
@@ -33,6 +33,8 @@ import psutil
 
 def cmd(s: str):
 	return Popen(s, shell=True).wait()
+
+
 def audio_block():
 	cmd(r'touch ~/.cache/ctwenty.lock')
 	cmd(r'i3-msg mode trap')
@@ -44,14 +46,15 @@ def audio_block():
 	cmd(r'i3-msg mode default')
 	cmd(r'rm -f ~/.cache/ctwenty.lock')
 
-def write_next_time(current_status : int, seconds: Optional[int] = None):
+
+def write_next_time(current_status: int, seconds: Optional[int] = None):
 	p = Path('~/.cache/ctwenty.target').expanduser()
 	if current_status == -1:
 		if p.exists():
 			p.unlink()
 	else:
 		assert seconds is not None
-		t = datetime.now() + timedelta(seconds = seconds)
+		t = datetime.now() + timedelta(seconds=seconds)
 		if current_status == 2:
 			emoji = "💛"
 		elif current_status == 1:
@@ -60,11 +63,17 @@ def write_next_time(current_status : int, seconds: Optional[int] = None):
 			emoji = "🤍"
 		p.write_text(f"{emoji}{t.strftime(':%M')}")
 
+
 # we need to make sure the signals don't end the program
-SIGNALS = [signal.SIGALRM, signal.SIGUSR1, signal.SIGTSTP,
-		signal.SIGINT, signal.SIGTERM, signal.SIGCONT]
+SIGNALS = [
+	signal.SIGALRM, signal.SIGUSR1, signal.SIGTSTP, signal.SIGINT, signal.SIGTERM, signal.SIGCONT
+]
+
+
 def noop(signum, frame):
 	print(signum, frame)
+
+
 for s in SIGNALS:
 	signal.signal(s, noop)
 
@@ -84,9 +93,11 @@ while True:
 	# update
 	if sig == signal.SIGINT or sig == signal.SIGTERM:
 		break
-	elif sig == signal.SIGTSTP or "i3lock" in (p.name() for p in psutil.process_iter()):
+	elif (
+		sig == signal.SIGTSTP or any(p.name().startswith("i3lock") for p in psutil.process_iter())
+	):
 		current_status = -1
-		signal.alarm(0) # clear any pending alarms
+		signal.alarm(0)  # clear any pending alarms
 	elif sig == signal.SIGUSR1 or current_status == 2:
 		audio_block()
 		current_status = 0
@@ -95,19 +106,23 @@ while True:
 	print("New state: ", current_status)
 
 	if current_status == -1:
-		t = 0 # wait until next i3lock
+		t = 0  # wait until next i3lock
 		write_next_time(-1)
 	elif current_status == 0:
 		t = 1000
 		write_next_time(0, 1337)
 	elif current_status == 1:
-		cmd(r'notify-send -i timer-symbolic -u low -t 120000 "Rest your eyes!" '\
-				r'"You have a mandatory break coming up soon in 6 minutes"')
+		cmd(
+			r'notify-send -i timer-symbolic -u low -t 120000 "Rest your eyes!" ' +
+			r'"You have a mandatory break coming up soon in 6 minutes"'
+		)
 		t = 300
 		write_next_time(1, 337)
 	elif current_status == 2:
-		cmd(r'notify-send -i timer-symbolic -t 30000 "Rest your eyes!" '\
-				r'"You have a mandatory break coming up soon in 37 seconds"')
+		cmd(
+			r'notify-send -i timer-symbolic -t 30000 "Rest your eyes!" ' +
+			r'"You have a mandatory break coming up soon in 37 seconds"'
+		)
 		t = 37
 		write_next_time(2, 37)
 	else:
