11c6f4d26060675ea556d1eb755eb48cc8f58760
Evan Chen <evan@evanchen.cc>
2021-09-06 00:48:21 -0400

    feat(skynet): flask server
    wow this is actually working

--- skynetsys/skynet.py
+++ skynetsys/skynet.py
@@ -1,33 +1,67 @@
+import os
 import subprocess
+from typing import Tuple
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask, redirect, render_template, request, session, url_for
 
 load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+VERBS = [
+	'status',
+	'start',
+	'stop',
+	'restart',
+]
+NOUNS = [
+	'evil-chin',
+	'mosp-2021',
+	'motion',
+	'dunst',
+]
+
+
+def check_status(noun: str) -> Tuple[str, str]:
+	cmd = ["systemctl", "is-active", "--quiet", "--user", f"{noun}.service"]
+	process = subprocess.run(cmd)
+	if process.returncode == 4:
+		return ('Not found', 'table-danger')
+	elif process.returncode == 3:
+		return ('Inactive', 'table-warning')
+	elif process.returncode == 0:
+		return ('Active', 'table-success')
+	else:
+		return ('Unknown', 'table-dark')
 
 
 @app.route("/")
 def index():
-	return render_template("base.html", title="SkyNet")
+	if session.get('logged_in'):
+		statuses = {noun: check_status(noun) for noun in NOUNS}
+		return render_template("index.html", title="SkyNet", statuses=statuses, verbs=VERBS)
+	else:
+		return render_template("login.html", title="SkyNet")
 
 
-@app.route("/mosp")
-def restart_mosp():
+@app.route("/login", methods=['POST'])
+def login():
+	if request.form['password'] == os.getenv('SKYNET_PASSWORD'):
+		session['logged_in'] = True
+	return redirect(url_for('index'))
+
+
+@app.route("/command/<verb>/<noun>/")
+def run_command(verb: str, noun: str):
+	if not session.get('logged_in'):
+		return redirect(url_for('index'))
+	assert verb in VERBS
+	assert noun in NOUNS
 	process = subprocess.run(
-		["systemctl", "restart", "mosp-2021.service", "--user"],
+		["systemctl", verb, f"{noun}.service", "--user"],
 		capture_output=True,
 		encoding='UTF-8',
 	)
-	return render_template("output.html", title="MOSP Bots", process=process)
-
-
-@app.route("/evil")
-def restart_evil():
-	process = subprocess.run(
-		["systemctl", "restart", "evil-chin.service", "--user"],
-		capture_output=True,
-		encoding='UTF-8',
-	)
-	return render_template("output.html", title="Evil Chin", process=process)
+	return render_template("output.html", title=f"{verb} {noun}", process=process)
