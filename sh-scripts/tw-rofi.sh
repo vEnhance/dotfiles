2398a1084ed90e7ddf5e6902d5a5fbee4656db94
Evan Chen <evan@evanchen.cc>
2022-02-15 19:02:23 -0500

    Drop header from tw-rofi, only sync if needed

--- sh-scripts/tw-rofi.sh
+++ sh-scripts/tw-rofi.sh
@@ -2,7 +2,7 @@
 
 default="$(task undone rc.defaultwidth=200 | sed -n 2p | sed 's/ /-/g')"
 lines="${default}\n$(task undone rc.gc=off rc.verbose=nothing rc.defaultwidth=200)"
-chosen=$(echo -e "$lines" | rofi -dmenu -i -p "taskwarrior")
+chosen=$(echo -e "$lines" | tail -n+2 | rofi -dmenu -i -p "taskwarrior")
 
 if [ -z "$chosen" ]; then
 	echo "Empty"
@@ -12,9 +12,11 @@ elif [[ "$lines" = *"$chosen"* ]]; then
 	taskid=$(echo $chosen | awk '{print $1}')
 	echo $taskid
 	task $taskid done
+	killall -s USR1 py3status
+	task sync
 else
 	task add $chosen
+	killall -s USR1 py3status
+	task sync
 fi
 
-killall -s USR1 py3status
-task sync
