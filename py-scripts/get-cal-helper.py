879819e9c32e8306e3f0a59b0828737df54ba912
Evan Chen <evan@evanchen.cc>
2022-05-22 17:07:03 -0400

    More guards against offline cal erasure

---
+++ py-scripts/get-cal-helper.py
@@ -0,0 +1,9 @@
+import json
+import sys
+
+json_data = json.load(sys.stdin)
+if 'error' not in json_data:
+	with open(sys.argv[1], 'w') as f:
+		print(json.dumps(json_data), file=f)
+else:
+	print("No data")
