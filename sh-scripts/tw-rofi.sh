9de66230b1a2125ff282fb6964578a8503349d63
Evan Chen <evan@evanchen.cc>
2022-09-09 13:01:43 -0400

    Simplify tw-rofi.sh and make choose by ID easier

--- sh-scripts/tw-rofi.sh
+++ sh-scripts/tw-rofi.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-default="$(task undone rc.defaultwidth=200 | sed -n 2p | sed 's/ /-/g')"
-lines="${default}\n$(task undone rc.gc=off rc.verbose=nothing rc.defaultwidth=200)"
-chosen=$(echo -e "$lines" | tail -n+2 | rofi -dmenu -i -p "taskwarrior")
+default="$(task undone rc.gc=off | sed -n 2p | sed 's/ /-/g' | sed 's/^ID/-ID-/')"
+lines="${default}\n$(task undone rc.gc=off rc.verbose=nothing | sed -r 's/^([0-9]+)/#\1;/' )"
+chosen=$(echo -e "$lines" | rofi -dmenu -i -p "taskwarrior")
 
 if [ -z "$chosen" ]; then
 	echo "Empty"
 elif [ "$chosen" = "$default" ]; then
 	echo "Default"
 elif [[ "$lines" = *"$chosen"* ]]; then
-	taskid=$(echo $chosen | awk '{print $1}')
+	taskid=$(echo $chosen | sed -r "s/[^ 0-9]//g" | awk '{print $1}')
 	echo $taskid
 	task $taskid done
 	killall -s USR1 py3status
 	task sync
 else
+	echo "sad"
+	exit
 	task add $chosen
 	killall -s USR1 py3status
 	task sync
 fi
-
