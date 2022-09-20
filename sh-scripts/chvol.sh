7acb3c0673722c8ab64ae818ac8f84ddc49fa381
Evan Chen <evan@evanchen.cc>
2022-09-20 08:47:17 -0400

    New controls and fixes for chvol.sh

--- sh-scripts/chvol.sh
+++ sh-scripts/chvol.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-time=600
-stime=800
+time=2000
+stime=4000
 
+# Mic volume control
 if [ "$1" = u ]; then
 	 notify-send -i microphone-sensitivity-high-symbolic \
 		"Mic volume up" \
@@ -18,19 +19,6 @@ if [ "$1" = i ]; then
 		"Mic volume reset" \
 		"Current mic volume $(ponymix -t source set-volume 90)%" -t $time
 fi
-
-if [ "$1" = k ]; then
-	 notify-send -i audio-volume-high \
-		"Global volume up" \
-		"Current volume $(ponymix increase 5)%" -t $time
-fi
-
-if [ "$1" = j ]; then
-	 notify-send -i audio-volume-medium \
-		"Global volume down" \
-		"Current volume $(ponymix decrease 5)%" -t $time
-fi
-
 if [ "$1" =  m ]; then
 	notify-send -i microphone-sensitivity-muted-symbolic \
 		"Microphone muted" \
@@ -42,52 +30,77 @@ if [ "$1" = w ]; then
 		"Microphone volume is $(ponymix -t source unmute)%" -t $time
 fi
 
+
+# Global volume control
+if [ "$1" = k ]; then
+	 notify-send -i audio-volume-high \
+		"Global volume up" \
+		"Current volume $(ponymix increase 5)%" -t $time
+fi
+if [ "$1" = j ]; then
+	 notify-send -i audio-volume-medium \
+		"Global volume down" \
+		"Current volume $(ponymix decrease 5)%" -t $time
+fi
+if [ "$1" =  M ]; then
+	notify-send -i audio-volume-muted-symbolic \
+		"Global volume muted" \
+		"Once was volume $(ponymix mute)%" -t $time
+fi
+if [ "$1" = N ]; then
+	notify-send -i audio-volume-low \
+		"Global volume unmuted" \
+		"Microphone volume is $(ponymix unmute)%" -t $time
+fi
+
+# Switch devices
 if [ "$1" = l ]; then
 	~/dotfiles/sh-scripts/paswitch.sh speakers
-	notify-send -i audio-volume-muted \
+	notify-send -i call-stop \
 		"Microphone muted, speakers on" \
 		"Once was volume $(ponymix -t source mute)%" -t $time
 	break
 fi
 if [ "$1" = r ]; then
 	~/dotfiles/sh-scripts/paswitch.sh usb
-	notify-send -i audio-input-microphone-high \
+	notify-send -i call-start \
 		"Microphone unmuted, speakers off" \
 		"Microphone volume is $(ponymix -t source unmute)%" -t $time
 	break
 fi
-
 if [ "$1" = s ]; then
 	~/dotfiles/sh-scripts/paswitch.sh speakers
-	notify-send -i audio-volume-muted \
+	notify-send -i audio-speakers \
 		"Switched to speakers" \
 		"Current volume is $(ponymix get-volume)%" -t $time
 	break
 fi
 if [ "$1" = h ]; then
 	~/dotfiles/sh-scripts/paswitch.sh usb
-	notify-send -i audio-input-microphone-high \
+	notify-send -i audio-headphones \
 		"Switched to headphones" \
 		"Microphone volume is $(ponymix get-volume)%" -t $time
 	break
 fi
 
+# Spotify stuff
 if [ "$1" = K ]; then
-	notify-send -i audio-headphones \
+	notify-send -i media-playback-start-symbolic \
 		"Spotify volume up" \
 		"Spotify volume $(ponymix -d Spotify --sink-input increase 5)%" -t $stime
 fi
 if [ "$1" = J ]; then
-	notify-send -i audio-headphones \
+	notify-send -i media-playback-start-rtl-symbolic \
 		"Spotify volume down" \
 		"Spotify volume $(ponymix -d Spotify --sink-input decrease 5)%" -t $stime
 fi
 if [ "$1" = z ]; then
-	notify-send -i audio-headphones \
+	notify-send -i edit-delete-symbolic \
 		"Spotify stream killed" \
 		"$(ponymix kill -d Spotify)%" -t $stime
 fi
 
+# playerctl play/pause prev next
 # wait a bit after playerctl to update py3status; race condition i think
 if [ "$1" = space ]; then
 	playerctl play-pause
@@ -98,7 +111,7 @@ if [ "$1" = p ]; then
 	sleep 0.1
 fi
 if [ "$1" = n ]; then
-	playerctl previous
+	playerctl next
 	sleep 0.1
 fi
 
