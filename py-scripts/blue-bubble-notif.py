0962d24ebab4f3a7b0966fd3c6b85dbeff3ebcbc
Evan Chen <evan@evanchen.cc>
2022-09-28 16:50:50 -0400

    blue bubbles monkey notif

---
+++ py-scripts/blue-bubble-notif.py
@@ -0,0 +1,37 @@
+import subprocess
+import sys
+
+sender, content = '', ''
+my_messages: list[str] = []
+
+for line in sys.stdin:
+	line = line.strip()
+	print(line)  # mirror the output
+
+	if not "[INFO]" in line:
+		continue
+	if not line.startswith("flutter"):
+		continue
+	if "Client received new" in line and ";" in line:
+		sender = line[line.rindex(";") + 1:]
+	elif "(MessageStatus) -> Message match: [" in line:
+		my_messages.append(line[95:-88])
+	elif "(Actions-HandleMessage) -> New message: [" in line:
+		if (x := line[101:-42]) in my_messages:
+			my_messages.remove(x)
+		else:
+			content = x
+
+	if sender != '' and content != '':
+		subprocess.call(
+			[
+				'notify-send',
+				'-i',
+				'chat-message-new-symbolic',
+				'-t',
+				'10000',
+				sender,
+				content,
+			]
+		)
+		sender, content = '', ''
