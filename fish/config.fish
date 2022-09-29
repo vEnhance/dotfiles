12450addfa3e90b8ffcd612db83e64ceb5f0b143
Evan Chen <evan@evanchen.cc>
2022-09-29 11:59:01 -0400

    config.fish: improve pdf command

--- fish/config.fish
+++ fish/config.fish
@@ -213,7 +213,10 @@ end
 
 # Shortcut for editors and the like
 function pdf
-	if test -f (echo $argv | cut -f 1 -d '.').pdf
+	string match '*.pdf' "$argv" > /dev/null
+	if test \( -f "$argv" \) -a \( $status -eq 0 \)
+		dn zathura $argv &> /dev/null
+	else if test -f (echo $argv | cut -f 1 -d '.').pdf
 		dn zathura (echo $argv | cut -f 1 -d '.').pdf &> /dev/null
 	else if test -f "$argv""pdf"
 		dn zathura "$argv""pdf" &> /dev/null
