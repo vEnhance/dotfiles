25bc3bb0c027944035895873f2f35a345de5aaeb
Evan Chen <evan@evanchen.cc>
2022-10-06 21:56:53 -0400

    Add yapf to lint.sh

--- lint.sh
+++ lint.sh
@@ -4,5 +4,7 @@ set -e # crash on any errors
 
 echo "Running pyflakes..."
 pyflakes $(git ls-files "*.py" | grep -v "qutebrowser/" | grep -v "ranger/")
+echo "Running yapf..."
+yapf -d $(git ls-files "*.py" | grep -v "qutebrowser/" | grep -v "ranger/")
 echo "Running spellcheck..."
 codespell $(git ls-files)
