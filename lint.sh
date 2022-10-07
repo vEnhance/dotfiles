71583a0a428e292b542f3f81a11b0d1706d984b6
Evan Chen <evan@evanchen.cc>
2022-10-07 14:47:01 -0400

    Finish shell lint.sh

--- lint.sh
+++ lint.sh
@@ -7,18 +7,17 @@ readarray -t PYTHON_FILES < <(git ls-files "**.py" | grep -v "qutebrowser/" | gr
 readarray -t SHELL_FILES < <(git ls-files "**.sh")
 
 # Spellcheck
-echo "Running spellcheck..."
+echo "Running spellcheck on ${#SPELL_FILES[@]} files..."
 codespell "${SPELL_FILES[@]}"
 
 # Python
-echo "Running pyflakes..."
+echo "Running pyflakes on ${#PYTHON_FILES[@]} files..."
 pyflakes "${PYTHON_FILES[@]}"
-echo "Running yapf..."
+echo "Running yapf on ${#PYTHON_FILES[@]} files..."
 yapf -d "${PYTHON_FILES[@]}"
 
 # Shell
-echo "Running shfmt..."
+echo "Running shfmt on ${#SHELL_FILES[@]} files..."
 shfmt -d "${SHELL_FILES[@]}"
-echo "Running shellcheck..."
-git ls-files "*.sh"
+echo "Running shellcheck on ${#SHELL_FILES[@]} files..."
 shellcheck --format=tty "${SHELL_FILES[@]}"
