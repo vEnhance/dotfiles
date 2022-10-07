4a25133a3f618332c29f9487af8d7cbd3f02f704
Evan Chen <evan@evanchen.cc>
2022-10-07 14:19:22 -0400

    Fix lint script

--- lint.sh
+++ lint.sh
@@ -2,9 +2,20 @@
 
 set -e # crash on any errors
 
-echo "Running pyflakes..."
-pyflakes "$(git ls-files "*.py" | grep -v "qutebrowser/" | grep -v "ranger/")"
-echo "Running yapf..."
-yapf -d "$(git ls-files "*.py" | grep -v "qutebrowser/" | grep -v "ranger/")"
+read -ra SPELL_FILES < <(git ls-files)
+read -ra PYTHON_FILES < <(git ls-files "*.py" | grep -v "qutebrowser/" | grep -v "ranger/")
+read -ra SHELL_FILES < <(git ls-files "*.sh")
+
+# Spellcheck
 echo "Running spellcheck..."
-codespell "$(git ls-files)"
+codespell "${SPELL_FILES[@]}"
+
+# Python
+echo "Running pyflakes..."
+pyflakes "${PYTHON_FILES[@]}"
+echo "Running yapf..."
+yapf -d "${PYTHON_FILES[@]}"
+
+# Shell
+echo "Running shfmt..."
+shfmt -d "${SHELL_FILES[@]}"
