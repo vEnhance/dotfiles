1702bbcc549b39814537d330954a7d9af96d5b1a
Evan Chen <evan@evanchen.cc>
2022-10-10 17:13:22 -0400

    More intelligent ll function

--- fish/config.fish
+++ fish/config.fish
@@ -359,12 +359,18 @@ if test (uname) = "Linux"
 	alias ls='ls --color=tty --quoting-style=literal'
 	alias la='ls -l --block-size=K'
 	function ll
-		set regex "(pythontex_data\.pkl|\.(aux|bbl|bcf|blg|fdb_latexmk|fls|log|maf|mtc0?|out|pre|ptc[0-9]+|pytxcode|pytxmcr|pytxpyg|run\.xml|toc|synctex\.gz|von)|-[0-9]{1,2}\.(asy|pdf))\$"
-		ls -l --block-size=K --color=yes $argv | grep -Ev $regex
-		set num_hidden (ls $argv | grep -E $regex | wc --lines)
+		if test (count *.tex) -eq 0
+			ls -l --block-size=K --color=yes $argv
+			return
+		end
+		set --function regex '('(string join '|' (string sub --end=-5 (string escape --style=regex *.tex)))')'
+		set --function regex $regex'\.(aux|bbl|bcf|blg|fdb_latexmk|fls|log|maf|mtc0?|out|pre|ptc[0-9]+|pytxcode|pytxmcr|pytxpyg|run\.xml|toc|synctex\.gz|von)|-[0-9]{1,2}\.(asy|pdf)$'
+		set --function regex '('$regex')|pythontex_data\.pkl'
+		set num_hidden (ls -l --block-size=K --color=yes $argv | grep -E $regex 2> /dev/null | wc --lines)
+		ls -l --block-size=K --color=yes $argv | grep -Ev $regex 2> /dev/null
 		if test $num_hidden -gt 0
 			echo (set_color cyan)"... and" (set_color --bold brgreen)$num_hidden(set_color normal)(set_color cyan) "garbage files not shown"(set_color normal)
-		end
+			end
 	end
 	function ranger-cd
 		if test -z "$RANGER_LEVEL"
