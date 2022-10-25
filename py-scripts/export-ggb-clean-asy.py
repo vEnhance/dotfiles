2aa973c8572f0996d7a4a8a7c5fc2f2c67727cee
Evan Chen <evan@evanchen.cc>
2022-10-24 21:28:05 -0400

    Vastly improve GGBasy cleanup script

--- py-scripts/export-ggb-clean-asy.py
+++ py-scripts/export-ggb-clean-asy.py
@@ -1,35 +1,50 @@
 #!/bin/python3
 
+import argparse
 import io
 import re
+import string
+import sys
 import traceback
 
 import pyperclip
 
-banner = "# Evan's GGB to Asy Cleanup script #"
-print("#" * len(banner))
-print("# Evan's GGB to Asy Cleanup script #")
-print("#" * len(banner))
-print()
+ALLOWED_CHARS = string.ascii_letters + string.digits + '_'
+
+ADVERTISEMENT = r"""/*
+	Converted from GeoGebra by User:Azjps using Evan's magic cleaner
+	https://github.com/vEnhance/dotfiles/blob/main/py-scripts/export-ggb-clean-asy.py
+*/
+"""
 
 fat_decimal_regex = re.compile(r'(\d+\.\d{5})\d+')
+
+parser = argparse.ArgumentParser(
+	description="Translates ugly GGB exported Asy to something more readable."
+)
+parser.add_argument('-s', '--speedy', action='store_true', help='Speedy mode')
+args = parser.parse_args()
 
 
 def replace_numbers(s):
 	return fat_decimal_regex.sub(r"\1", s)
 
 
-clipboard_contents = pyperclip.paste()
-try:
-	input_buffer = io.StringIO(clipboard_contents)
-	output_buffer = io.StringIO()
+if args.speedy:
+	input_contents = pyperclip.paste()
+else:
+	input_contents = ''.join(sys.stdin.readlines())
 
+try:
+	input_buffer = io.StringIO(input_contents)  # replace with clipboard
 	first_line = input_buffer.readline()
+
+	output_buffer = io.StringIO()
 	assert "Geogebra to Asymptote conversion" in first_line, f"First line is missing header\n{first_line}"
 	# print(r'/* start ggb to asy preamble */', file=output_buffer)
-	points_dict = {}
+	point_coords_dict = {}
 
-	# preamble
+	# Preamble
 	for line in input_buffer:
 		line = replace_numbers(line).strip()
 		if line == "" or r"/* draw figures */" in line:  # end preamble
@@ -48,13 +63,13 @@ try:
 			content = line[5:].strip().rstrip(';')
 			assignments = content.split(', ')
 			for assn in assignments:
-				point_name, pair = assn.split(' = ')
-				points_dict[point_name] = eval(pair)
+				point_name, point_coords = assn.split(' = ')
+				point_coords_dict[point_name] = eval(point_coords)
 		for statement in line.split(';'):
 			if statement.strip():
 				print(statement.strip() + ';', file=output_buffer)
 
-	# process figures
+	# Process figures
 	for line in input_buffer:
 		line = replace_numbers(line).strip()
 		if r"dots and labels" in line:  # end figures
@@ -66,11 +81,15 @@ try:
 		line = line.replace("linewidth(2.)", "linewidth(0.6)")
 		print(line.strip(), file=output_buffer)
 
-	# process dots and labels
+	# Get the figure output, modulo point labelling
+	figures_output_code = output_buffer.getvalue() + '\n'
+
+	# Process dots and labels
 	dot_regex = re.compile(r'dot\(([a-zA-Z0-9_]+|\([0-9\.\-,]+\)),.*?dotstyle\);')
 	label_regex = re.compile(r'label\("(.+?)", (\([0-9\.\-,]+\)), .+?\);')
-
+	label_to_coords = {}
 	while True:
+		# Reads lines in pairs, two at a time
 		dot_line = replace_numbers(input_buffer.readline().strip())
 		if dot_line == r"/* end of picture */" or dot_line == '':  # end of file
 			break
@@ -81,33 +100,58 @@ try:
 		label_match = label_regex.match(label_line)
 		assert label_match is not None, label_line
 
-		point = dot_match.groups()[0]
+		point_coords = dot_match.groups()[0]
 		label = label_match.groups()[0]
 		label_loc = eval(label_match.groups()[1])
-		# determine the direction
-		if point in points_dict:
-			coords = points_dict[point]
+		if point_coords in point_coords_dict:
+			coords = point_coords_dict[point_coords]
 		else:
-			assert all(_ in '.0123456789-,()' for _ in point)
-			coords = eval(point)
+			assert all(_ in '.0123456789-,()' for _ in point_coords)
+			coords = eval(point_coords)
 
 		if coords is None:
-			print(r'dot("%s", %s, dir(45));' % (label, point), file=output_buffer)
+			figures_output_code += r'dot("%s", %s, dir(45));' % (label, point_coords) + '\n'
 		else:
+			# determine the direction
 			dx = 100 * (label_loc[0] - coords[0])
 			dy = 100 * (label_loc[1] - coords[1])
-			print(r'dot("%s", %s, dir((%.3f, %.3f)));' % (label, point, dx, dy), file=output_buffer)
+			figures_output_code += r'dot("%s", %s, dir((%.3f, %.3f)));' % (
+				label, point_coords, dx, dy
+			) + '\n'
+		label_to_coords[label] = point_coords
 
-	output = output_buffer.getvalue()
-	print(output[:500] + '\n...\n' + output[-500:])
-	response = input("Replace clipboard contents? [y/n] ")
-	if response.strip().lower() == 'y':
-		pyperclip.copy(output)
+	# now clean up the code if possible by replacing explicit coordinates where we can
+	used_point_var_names: list[str] = []
+	pair_declarations = ''
+	for label, point_coords in label_to_coords.items():
+		if label[0] != '$' or label[-1] != '$':
+			continue
+		label = label[1:-1]
+		label = label.replace("'", "p")
+		point_var_name = ''.join(_ for _ in label if _ in ALLOWED_CHARS)
+		while point_var_name in used_point_var_names:
+			if (d := point_var_name[-1]).isdigit() and d != 9:
+				point_var_name = point_var_name[:-1] + str(int(d) + 1)
+			else:
+				point_var_name += '_0'
+		figures_output_code = figures_output_code.replace(str(point_coords), label)
+		pair_declarations += f'pair {label} = {point_coords};' + '\n'
+
+	output = ADVERTISEMENT + pair_declarations + '\n' + figures_output_code
+
+	# print finished output
+	output = output.strip()
+	if args.speedy:
+		print(pair_declarations[:400] + '...\n' + figures_output_code[:600] + '...\n')
+		response = input("Replace clipboard contents? [y/n] ")
+		if response.strip().lower() == 'y':
+			pyperclip.copy(output)
+	else:
+		print(output)
 
 except:
-	print("Input:")
-	print()
-	print(clipboard_contents[0:500] + '...')
-	print('-' * 40 + '\n')
+	print(input_contents[0:500])
+	print('-' * 50)
 	traceback.print_exc()
-	response = input("Failed. Press ENTER to continue... ")
+	if args.speedy:
+		response = input("Failed. Press ENTER to continue... ")
