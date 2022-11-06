b1bf4a42287d174952fadeb1c6824342cf6fe95f
Evan Chen <evan@evanchen.cc>
2022-11-06 08:25:01 -0500

    Print todo before calendar

--- conky/print-conky-agenda.sh
+++ conky/print-conky-agenda.sh
@@ -11,8 +11,8 @@ python ~/dotfiles/conky/gcal-json-to-text.py ~/.cache/agenda.txt <"$HOME"/.cache
 color_num=(8 4 4 5 5 5 6 6 6 6 6 6)
 
 for k in {1..12}; do
-	if [ "$(wc --lines <~/.cache/agenda.txt)" -ge "$k" ]; then
-		echo "\${color${color_num[k - 1]}}$(tail ~/.cache/agenda.txt -n +"${k}" | head -n 1)"
+	if [ "$(wc --lines <~/.cache/todo.txt)" -ge "$k" ]; then
+		echo "\${color${color_num[k - 1]}}$(tail ~/.cache/todo.txt -n +"${k}" | head -n 1)"
 	fi
 done
 
@@ -20,7 +20,7 @@ done
 echo '${voffset -10}${color0}${stippled_hr}${voffset -2}'
 
 for k in {1..12}; do
-	if [ "$(wc --lines <~/.cache/todo.txt)" -ge "$k" ]; then
-		echo "\${color${color_num[k - 1]}}$(tail ~/.cache/todo.txt -n +"${k}" | head -n 1)"
+	if [ "$(wc --lines <~/.cache/agenda.txt)" -ge "$k" ]; then
+		echo "\${color${color_num[k - 1]}}$(tail ~/.cache/agenda.txt -n +"${k}" | head -n 1)"
 	fi
 done
