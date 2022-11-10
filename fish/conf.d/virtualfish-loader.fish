1a2cfc9bc6f52fc4f3a932953a1f29e23924db38
Evan Chen <evan@evanchen.cc>
2022-11-10 17:02:26 -0500

    zzz use system virtualfish

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,5 +1,5 @@
 set -g VIRTUALFISH_VERSION 2.5.5
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /home/evan/.local/lib/python3.10/site-packages/virtualfish/virtual.fish
-source /home/evan/.local/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+source /usr/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.10/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
