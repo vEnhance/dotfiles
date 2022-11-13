629e4946554403f75b8c99bf59ea268d3a776a59
Evan Chen <evan@evanchen.cc>
2022-11-13 17:28:31 -0500

    gh: How can adding labels to an issue be so hard

--- fish/config.fish
+++ fish/config.fish
@@ -226,7 +226,7 @@ function newtex
 	echo '\usepackage{evan}' >> "$argv.tex"
 	echo '\begin{document}' >> "$argv.tex"
 	echo '\title{}' >> "$argv.tex"
-	echo '<++>' >> "$argv.tex"
+	echo '' >> "$argv.tex"
 	echo '\end{document}' >> "$argv.tex"
 	gvim "$argv.tex"
 end
@@ -289,39 +289,46 @@ function lt
 	j-langtool $argv | cut -c 1-80 | bat -l verilog --wrap=never --paging=never
 end
 
-# github CLI extension
+# github CLI wrapper
 function hub
 	set -l digits (echo $argv | ag --only-matching "[0-9]+" --nocolor)
+	# first decide if we are a PR or an issue
+	if test -n "$digits"
+		gh issue view $digits &> /dev/null
+		if test $status -eq 0
+			set flavor "issue"
+		else
+			set flavor "pr"
+		end
+	end
 	if test -z "$argv" -o (echo $argv | cut -c 1) = "-"
-		gh issue list $argv
-		gh pr list $argv
+		gh issue list $argv --limit 10
+		gh pr list $argv --limit 10
+	else if test "$argv" = "issue"
+		gh issue list
 	else if test "$argv" = "pr"
 		gh pr list
+	else if test "$argv" = "claim $digits"
+		gh $flavor edit --add-assignee "@me"
+	else if test "$argv" = "lb $digits"
+		GH_FORCE_TTY=50% gh $flavor view $digits | head -n 8
+		set_color --bold cyan
+		echo "Choose a label to add..."
+		set_color normal
+		gh $flavor edit $digits --add-label (GH_FORCE_TTY='50%' gh label list | fzf --height 15 --ansi --header-first --header-lines 3 | sed "s/  /\t/" | cut -f 1)
 	else if test "$argv" = "re $digits"
-		gh issue view --comments $digits 2> /dev/null
-		if test $status -eq 0
-			echo "Enter your comment below, or blank to open Vi..."
-			cat > /tmp/gh-comment.txt
-			if ag "[^\s]+" /tmp/gh-comment.txt > /dev/null
-				gh issue comment $digits -F /tmp/gh-comment.txt
-			else
-				gh issue comment $digits
-			end
+		gh $flavor view --comments $digits
+		set_color --bold yellow
+		echo "Enter your comment below, or blank to open Vi..."
+		set_color normal
+		cat > /tmp/gh-comment.txt
+		if ag "[^\s]+" /tmp/gh-comment.txt > /dev/null
+			gh $flavor comment $digits -F /tmp/gh-comment.txt
 		else
-			gh pr view --comments $digits
-			echo "Enter your comment below, or blank to open Vi..."
-			cat > /tmp/gh-comment.txt
-			if ag "[^\s]+" /tmp/gh-comment.txt > /dev/null
-				gh pr comment $digits -F /tmp/gh-comment.txt
-			else
-				gh pr comment $digits
-			end
+			gh $flavor comment $digits
 		end
 	else if test (echo $argv | cut -d " " -f 1) = "$digits"
-		gh issue view $argv 2> /dev/null
-		if test $status -ne 0
-			gh pr view $argv
-		end
+		hub $flavor view $argv
 	else
 		gh $argv
 	end
