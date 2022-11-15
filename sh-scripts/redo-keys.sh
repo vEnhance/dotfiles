c2cbe3828947a41c56ab712a6c4aff5b5b33a743
Evan Chen <evan@evanchen.cc>
2022-11-14 22:07:16 -0500

    Run xmodmap -e "remove lock = Caps_Lock" last
    Sometimes it crashes

--- sh-scripts/redo-keys.sh
+++ sh-scripts/redo-keys.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
 # Make sure we're in Dvorak
-xmodmap -e "remove lock = Caps_Lock"
 setxkbmap dvorak -option caps:escape
 numlockx on
+
+xmodmap -e "remove lock = Caps_Lock"
 
 # synclient TapButton1=0           # Disable tap to click
 # synclient TapButton2=0           # Disable double tap to paste
