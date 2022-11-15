cecdcb1786717af4386ab5c6d81f568886965668
Evan Chen <evan@evanchen.cc>
2022-11-14 22:19:59 -0500

    Improve reminder-exec.sh display

--- i3/reminder-exec.sh
+++ i3/reminder-exec.sh
@@ -4,23 +4,16 @@ notify-send "i3: exec mode" \
 	-i categories/applications-utilities-symbolic \
 	-t 4000 \
 	-u low \
-	"<b>a</b>: asy parser
-<b>b</b>: libreoffice
-<b>c</b>: caprine
-<b>d</b>: discord
-<b>f</b>: firefox
+	"<b>a</b>: asy                <b>b</b>: libreoffice
+<b>c</b>: caprine         <b>d</b>: discord
+<b>e/f</b>: epiphany/firefox
 <b>g</b>: geogebra
 <b>h</b>: chromium (hanabi)
 <b>l</b>: lock
-<b>m</b>: gnucash
-<b>n</b>: vit
-<b>o</b>: obs
-<b>p</b>: projtext
-<b>q</b>: qutebrowser
-<b>r</b>: ORCH
-<b>s</b>: spotify
-<b>v</b>: venueQ
-<b>y</b>: diary
-<b>z</b>: zoom
+<b>m</b>: gnucash          <b>n</b>: vit
+<b>o</b>: obs              <b>p</b>: projtext
+<b>q</b>: qutebrowser      <b>r</b>: ORCH
+<b>s</b>: spotify          <b>v</b>: venueQ
+<b>y</b>: diary            <b>z</b>: zoom
 <b>Space</b>: loginctl lock
 "
