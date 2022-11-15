7dfbef18caddabce18d547c75c7f3b8cdc2ec2cd
Evan Chen <evan@evanchen.cc>
2022-11-14 22:22:25 -0500

    Change i3 shortcuts

--- i3/reminder-exec.sh
+++ i3/reminder-exec.sh
@@ -12,8 +12,8 @@ notify-send "i3: exec mode" \
 <b>l</b>: lock
 <b>m</b>: gnucash          <b>n</b>: vit
 <b>o</b>: obs              <b>p</b>: projtext
-<b>q</b>: qutebrowser      <b>r</b>: ORCH
-<b>s</b>: spotify          <b>v</b>: venueQ
+<b>q</b>: qutebrowser      <b>r</b>: oRch
+<b>s</b>: spotify          <b>t</b>: oTis
 <b>y</b>: diary            <b>z</b>: zoom
 <b>Space</b>: loginctl lock
 "
