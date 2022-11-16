ad609e144a18e0795ddd33a3ff4b4d64f7c003e0
Evan Chen <evan@evanchen.cc>
2022-11-16 18:11:27 -0500

    Note for Fish

--- sh-scripts/save-bitwarden-password.fish
+++ sh-scripts/save-bitwarden-password.fish
@@ -1,8 +1,9 @@
 #!/bin/fish
 
+# for bash use read -sp instead of read -s -P
 read -s -P "BitWarden Password: " BITWARDEN_MASTER_PASSWORD
 read -s -P "User PIN: " USER_PIN
 
-echo $BITWARDEN_MASTER_PASSWORD \
-    | openssl aes-256-cbc -a -salt -pbkdf2 -pass pass:$USER_PIN \
+echo "$BITWARDEN_MASTER_PASSWORD" \
+    | openssl aes-256-cbc -a -salt -pbkdf2 -pass "pass:$USER_PIN" \
     | secret-tool store --label="BitWarden Vault" type bitwarden user local
