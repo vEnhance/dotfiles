1f8588b08e9e8e797b2f4ea2f717d47cc3202632
Evan Chen <evan@evanchen.cc>
2022-11-17 11:56:44 -0500

    bitwarden helper polish
    see why store in clipboard if you can just type it

--- sh-scripts/grab-password-from-vault.sh
+++ sh-scripts/grab-password-from-vault.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
+echo "+=========================================+"
+echo "| Welcome to the BITWARDEN Vault grabber! |"
+echo "| (yet another hack written by Evan Chen) |"
+echo "+=========================================+"
+
 if test -z "$BW_SESSION"; then
-  USER_PIN=$(rofi -dmenu -p "Enter the PIN" -password -lines 0 --location 2)
+  read -r -s -p "Enter the PIN [echo hidden]: " USER_PIN
+  echo -e "\n-------------------------------------------"
   if test -z "$USER_PIN"; then
-    echo "No PIN was entered"
+    notify-send -u critical -t 3000 -i 'status/security-high-symbolic' 'No PIN' \
+      'No PIN was entered to get the BitWarden master password.'
     exit 1
+    exit 1
+  else
+    echo ""
+    echo "Received PIN from the command line."
   fi
 
   if ! MASTER_PASSWORD=$(secret-tool lookup type bitwarden user local |
     openssl aes-256-cbc -a -d -pbkdf2 -pass "pass:$USER_PIN"); then
-    notify-send -u critical -t 5000 -i "status/dialog-password" "Wrong PIN" \
-      "Either that or master password retrieval failed"
+    notify-send -u critical -t 5000 -i 'status/security-high-symbolic' 'Wrong PIN' \
+      'Either that or master password retrieval failed'
     exit 1
+  else
+    echo -e "\033[42mAuthentication successful!\033[0m"
+    echo -e "Retrieving data from vault..."
   fi
   BW_SESSION=$(bw unlock "$MASTER_PASSWORD" --raw)
 fi
 
 readarray -t BW_LIST < <(bw list items --pretty --session "$BW_SESSION")
 if [ "${#BW_LIST[@]}" -eq 0 ]; then
-  notify-send -i 'status/dialog-password' -u critical -t 3000 \
-    "Vault retrieval failed" \
-    "We could not access the BitWarden vault."
+  notify-send -i 'status/security-high-symbolic' -u critical -t 3000 \
+    'Vault retrieval failed' \
+    'We could not access the BitWarden vault.'
   exit 1
 fi
 
 TARGET_ROW="$(echo "${BW_LIST[@]}" |
   jq -C '.[]|{name, user:.login.username, favorite, notes, password_updated:.login.passwordRevisionDate, last_updated:.login.revisionDate, id}' -r --compact-output |
-  fzf -d "," --nth 1,2,3,4 --with-nth 1,2 --tac --ansi --preview 'echo {} | jq -C')"
+  fzf --height 36 -d ',' --nth 1,2,3,4 --with-nth 1,2 --tac --ansi --preview 'echo {} | jq -C')"
 if test -n "$TARGET_ROW"; then
   TARGET_ID="$(echo "$TARGET_ROW" | jq -r ".id")"
   TARGET_PASSWORD="$(echo "${BW_LIST[@]}" | jq ".[]|select(.id == \"$TARGET_ID\")|.login.password" -r)"
@@ -36,15 +50,15 @@ fi
 
 if test -n "$TARGET_PASSWORD"; then
   echo -n "$TARGET_PASSWORD" | xsel --secondary
-  notify-send -i 'status/dialog-password' -u low -t 5000 \
+  notify-send -i 'status/security-high-symbolic' -u low -t 5000 \
     "Password copied to secondary clipboard" \
     "$(echo "$TARGET_ROW" | jq -C '.user + " at " + .name' -r), valid for 30 seconds"
   sleep 30 && xsel --secondary --clear &
-  bw lock
+  # sleep 0.2 && xdotool type "$TARGET_PASSWORD" && bw lock &
   exit 0
 else
-  notify-send -i 'status/dialog-password' -u critical -t 3000 \
-    "Password grab failed" \
-    "We could not get a password."
+  notify-send -i 'status/security-high-symbolic' -u critical -t 3000 \
+    'Password grab failed' \
+    'We could not get a password.'
   exit 0
 fi
