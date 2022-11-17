53852494c638db118dd81ee7dcd11159d63bd4d8
Evan Chen <evan@evanchen.cc>
2022-11-17 00:47:44 -0500

    Rename SC2Replays script

---
+++ sh-scripts/rename-sc2replays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for filename in ./*.SC2Replay; do
+  if [[ $filename == vEnhance* ]]; then
+    echo "Already renamed $filename"
+  else
+    BASENAME="$(basename "$filename" | cut -f 1 -d '.' | cut -f 1 -d '(' | xargs)"
+    TIMESTAMP="$(stat -c %W "$filename" | date +"%F %H%M%S")"
+    mv "$filename" "vEnhance $BASENAME $TIMESTAMP.SC2Replay"
+  fi
+done
