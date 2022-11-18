41e7a22e6942507f24ddd690c1924d852edfeae7
Evan Chen <evan@evanchen.cc>
2022-11-18 11:21:12 -0500

    Switch to password vault on p

--- i3/reminder-exec.sh
+++ i3/reminder-exec.sh
@@ -5,14 +5,14 @@ notify-send "i3: exec mode" \
   -t 4000 \
   -u low \
   "<b>a</b>: asy                <b>b</b>: libreoffice
-<b>c</b>: caprine         <b>d</b>: discord
-<b>e</b>: epiphany        <b>g</b>: geogebra
-<b>h</b>: cHromium         <b>l</b>: lock
-<b>m</b>: gnucash          <b>n</b>: vit
-<b>o</b>: obs              <b>p</b>: projtext
-<b>q</b>: qutebrowse       <b>r</b>: oRch
-<b>s</b>: spotify          <b>t</b>: oTis
-<b>v</b>: vault            <b>x</b>: firefoX
+<b>c</b>: caprine          <b>d</b>: discord
+<b>e</b>: epiphany         <b>g</b>: geogebra
+<b>h</b>: cHromium         <b>j</b>: projtext
+<b>l</b>: lock             <b>m</b>: gnucash
+<b>n</b>: vit              <b>o</b>: obs
+<b>p</b>: vault            <b>q</b>: qutebrowser
+<b>r</b>: oRch             <b>s</b>: spotify
+<b>t</b>: oTis             <b>x</b>: firefoX
 <b>y</b>: diarY            <b>z</b>: zoom
 <b>Space</b>: loginctl lock
 "
