d1e657371f83be9f206ed8908c16759d75802942
Evan Chen <evan@evanchen.cc>
2022-11-18 19:39:08 -0500

    config.fish: Better gh re

--- fish/config.fish
+++ fish/config.fish
@@ -332,7 +332,7 @@ function hub
         set_color normal
         gh $flavor edit $digits --add-label (GH_FORCE_TTY='50%' gh label list | fzf --height 15 --ansi --header-first --header-lines 3 | sed "s/  /\t/" | cut -f 1)
     else if test "$argv" = "re $digits"
-        gh $flavor view --comments $digits
+        GH_FORCE_TTY=1 GH_PAGER=cat gh $flavor view --comments $digits | awk -v n=4 'NR <= n { print; next } { delete lines[NR - n]; lines[NR] = $0 } END { for(i = NR - n + 1; i <= NR; ++i) print lines[i] }'
         set_color --bold yellow
         echo "Enter your comment below, or blank to open Vi..."
         set_color normal
