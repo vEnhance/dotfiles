4e50df1dd74f91c4021a8c225f51f891efe78ec8
Evan Chen <evan@evanchen.cc>
2022-11-19 22:26:06 -0500

    Improved bw-unlock function using PIN

--- fish/config.fish
+++ fish/config.fish
@@ -176,8 +176,22 @@ function aes-decode
     openssl aes-256-cbc -a -d -pbkdf2 -pass pass:$argv
 end
 function bw-unlock
-    export BW_SESSION=(bw unlock --raw)
-    bw status --pretty | jq
+    set_color brpurple
+    echo "Enter PIN to continue (or leave blank if none):"
+    set_color normal
+    read -s -P "[echo hidden]: " USER_PIN
+
+    set MASTER_PASSWORD ""
+    if test -n "$USER_PIN"
+        set MASTER_PASSWORD (secret-tool lookup type bitwarden user local |
+            openssl aes-256-cbc -a -d -pbkdf2 -pass "pass:$USER_PIN")
+    end
+    if test -z "$MASTER_PASSWORD"
+        export BW_SESSION=(bw unlock --raw)
+    else
+        export BW_SESSION=(bw unlock "$MASTER_PASSWORD" --raw)
+    end
+    bw status | jq
 end
 
 #gd ubuntu
