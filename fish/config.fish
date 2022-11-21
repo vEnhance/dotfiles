c6601be1ae4e0aae0c4c81423fe064d982730b14
Evan Chen <evan@evanchen.cc>
2022-11-21 18:31:57 -0500

    rs1 and rs2 aliases

--- fish/config.fish
+++ fish/config.fish
@@ -224,6 +224,26 @@ function bw-new
     echo $item | bw encode | bw create item | jq
 end
 
+function rs1
+    set regex "[abe-hjkn-uwyz]*"(
+        echo $argv[1] |
+        string lower |
+        string split '' |
+        string join '[abe-hjkn-uwyz]*'
+    )"[abe-hjkn-uwyz]*"
+    ag -w $regex $argv[2..]
+end
+
+function rs2
+    set regex "[abe-hjkn-uwyz]*"(
+        echo $argv[1] |
+        string lower |
+        string split '' |
+        string join '[abe-hjkn-uwyz ]*'
+    )"[abe-hjkn-uwyz]*"
+    ag -w $regex $argv[2..]
+end
+
 #gd ubuntu
 alias pip='pip3'
 alias pudb='pudb3'
