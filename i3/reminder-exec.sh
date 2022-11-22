81e97d01e9b6b12ad6668600c4d07f7d09096e00
Evan Chen <evan@evanchen.cc>
2022-11-22 14:54:05 -0500

    i3: Toggle browser keybinds

--- i3/reminder-exec.sh
+++ i3/reminder-exec.sh
@@ -6,13 +6,13 @@ notify-send "i3: exec mode" \
   -u low \
   "<b>a</b>: asy                <b>b</b>: libreoffice
 <b>c</b>: caprine          <b>d</b>: discord
-<b>e</b>: epiphany         <b>g</b>: geogebra
-<b>h</b>: cHromium         <b>j</b>: projtext
-<b>l</b>: lock             <b>m</b>: gnucash
-<b>n</b>: vit              <b>o</b>: obs
-<b>p</b>: vault            <b>q</b>: qutebrowser
+<b>e</b>: epiphany         <b>f</b>: firefox
+<b>g</b>: geogebra         <b>h</b>: cHromium
+<b>j</b>: projtext         <b>l</b>: lock
+<b>m</b>: gnucash          <b>n</b>: vit
+<b>o</b>: obs              <b>p</b>: vault
 <b>r</b>: oRch             <b>s</b>: spotify
-<b>t</b>: oTis             <b>x</b>: firefoX
+<b>t</b>: oTis             <b>x</b>: qutebrowser
 <b>y</b>: diarY            <b>z</b>: zoom
 <b>Space</b>: loginctl lock
 "
