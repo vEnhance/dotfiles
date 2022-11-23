38216328b36d0cf97f2487a638ef48429fd03cc0
Evan Chen <evan@evanchen.cc>
2022-11-22 23:06:39 -0500

    qutebrowser: whitelist drive.google.com

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -64,6 +64,7 @@ ALLOW_JAVASCRIPT_WEBSITES = (
     r'*://calendar.google.com/*',
     r'*://calendly.com/*',
     r'*://discord.com/*',
+    r'*://drive.google.com/*',
     r'*://duckduckgo.com/*',
     r'*://github.com/*',
     r'*://hanab.live/*',
