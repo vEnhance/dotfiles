7f869aa9930b9beeaa92444a6d83e53db2d300dd
Evan Chen <evan@evanchen.cc>
2022-12-17 01:20:48 -0500

    FUCKKK BASH

--- sh-scripts/rename-sc2replays.sh
+++ sh-scripts/rename-sc2replays.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -o xtrace
+set -e
+
 for filename in ./*.SC2Replay; do
   if [[ $filename == *vEnhance* ]]; then
     echo "Already renamed $filename"
   else
     BASENAME="$(basename "$filename" | cut -f 1 -d '.' | cut -f 1 -d '(' | xargs)"
-    TIMESTAMP="$(stat -c %W "$filename" | date +"%F %H%M%S")"
-    mv "$filename" "vEnhance $BASENAME $TIMESTAMP.SC2Replay"
+    TIMESTAMP=$(date +"%F %H%M%S" -d "@$(stat -c "%W" "$filename")")
+    mv --no-clobber "$filename" "vEnhance $BASENAME $TIMESTAMP.SC2Replay"
   fi
 done
