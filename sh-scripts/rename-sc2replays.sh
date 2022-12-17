19cd22a72bd3f5b81390ab3938495bcf6e5c375c
Evan Chen <evan@evanchen.cc>
2022-12-17 00:47:40 -0500

    Fix autodetect already rename

--- sh-scripts/rename-sc2replays.sh
+++ sh-scripts/rename-sc2replays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for filename in ./*.SC2Replay; do
-  if [[ $filename == vEnhance* ]]; then
+  if [[ $filename == *vEnhance* ]]; then
     echo "Already renamed $filename"
   else
     BASENAME="$(basename "$filename" | cut -f 1 -d '.' | cut -f 1 -d '(' | xargs)"
