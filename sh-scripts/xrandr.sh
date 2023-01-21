dd1c3151e30da9fbdf8976ca8de4169306fff396
Evan Chen <evan@evanchen.cc>
2023-01-20 23:15:27 -0800

    xrandr output names changed?? wtf

--- sh-scripts/xrandr.sh
+++ sh-scripts/xrandr.sh
@@ -30,18 +30,11 @@ if [ "$(hostname)" = ArchMajestic ]; then
 fi
 
 if [ "$(hostname)" = ArchBootes ]; then
-  # why do i need to turn DP-1 off first wtf is this
   xrandr \
-    --output DP-1 --off \
+    --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
     --output DP-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
-    --output DP-3 --mode 2560x1440 --pos 4480x0 --rotate normal \
-    --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-    ;
-  xrandr \
-    --output DP-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
-    --output DP-3 --mode 2560x1440 --pos 4480x0 --rotate normal \
-    --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DP-1 --mode 2560x1440 --pos 7040x0 \
+    --output DP-0 --mode 2560x1440 --pos 4480x0 --rotate normal \
+    --output DP-4 --mode 2560x1440 --pos 7040x0 \
     ;
 fi
 
