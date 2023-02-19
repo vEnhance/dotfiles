d647e420217a99bc229127821bccf4408f77fa47
Evan Chen <evan@evanchen.cc>
2023-02-19 13:19:39 -0800

    conky: ArchDiamond is on a TV always now

--- conky/run-conky.sh
+++ conky/run-conky.sh
@@ -10,13 +10,8 @@ if [ "$(hostname)" = ArchSapphire ]; then
 fi
 if [ "$(hostname)" = ArchDiamond ]; then
   if [ "$(whoami)" = "evan" ]; then
-    if [ "$(date +'%Z')" = "EDT" ] || [ "$(date +'%Z')" = "EST" ]; then
-      conky -c ~/dotfiles/conky/thin-bar-3840x2160.conf &
-      conky -c ~/dotfiles/conky/cal5.conf &
-    else
-      conky -c ~/dotfiles/conky/thin-bar-1920x1080.conf &
-      conky -c ~/dotfiles/conky/cal3.conf &
-    fi
+    conky -c ~/dotfiles/conky/thin-bar-3840x2160.conf &
+    conky -c ~/dotfiles/conky/cal5.conf &
   fi
 fi
 if [ "$(hostname)" = ArchMajestic ]; then
