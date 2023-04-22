5e14b3f0a5cee063d86fefb3ee479a1f878d3d62
Evan Chen <evan@evanchen.cc>
2023-04-22 11:36:30 -0700

    feat: reset trackpad on laptop

--- sh-scripts/redo-keys.sh
+++ sh-scripts/redo-keys.sh
@@ -19,6 +19,10 @@ fi
 
 xmodmap -e "remove lock = Caps_Lock"
 
+if [ "$(hostname)" = ArchScythe ]; then
+  synclient VertScrollDelta=-237
+fi
+
 # synclient TapButton1=0           # Disable tap to click
 # synclient TapButton2=0           # Disable double tap to paste
 # synclient RightButtonAreaRight=1 # Remap mouse buttons
