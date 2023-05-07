3fbb34f1afe149e8269d4b9310e99925cfb25fb6
Evan Chen <evan@evanchen.cc>
2023-05-07 12:57:52 -0700

    VirtualFish now on python3.11

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,5 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.5
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /usr/lib/python3.10/site-packages/virtualfish/virtual.fish
-source /usr/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+source /usr/lib/python3.11/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
