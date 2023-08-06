386fdd6a2332e6011d9e5cb9ca86696c9d57869e
Evan Chen <evan@evanchen.cc>
2023-08-06 14:41:27 -0700

    feat: new script to grab credit card numbers
    Remembering these is so annoying

--- i3/reminder-exec.sh
+++ i3/reminder-exec.sh
@@ -9,7 +9,7 @@ notify-send "i3: exec mode" \
 <b>g</b>: geogebra         <b>h</b>: cHromium
 <b>j</b>: projtext         <b>l</b>: lock
 <b>m</b>: gnucash          <b>n</b>: vit
-<b>o</b>: obs              <b>p</b>: vault
+<b>o</b>: obs              <b>p/c</b>: vault
 <b>r</b>: oRch             <b>s</b>: spotify
 <b>t</b>: oTis             <b>x</b>: qutebrowser
 <b>y</b>: diarY            <b>z</b>: zoom
--- sh-scripts/grab-password-from-vault.sh
+++ sh-scripts/grab-password-from-vault.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "+=========================================+"
-echo -e "| Welcome to the \033[1;36mBITWARDEN Vault grabber\033[m! |"
+if [ "$1" = "cards" ]; then
+  echo -e "| Welcome to the \033[1;32mBITWARDEN Cards grabber\033[m! |"
+else
+  echo -e "| Welcome to the \033[1;36mBITWARDEN Vault grabber\033[m! |"
+fi
 echo -e "| (yet another hack written by \033[1;33mEvan Chen\033[m) |"
 echo "+=========================================+"
 
@@ -33,6 +37,7 @@ if test -z "$BW_SESSION"; then
 fi
 
 readarray -t BW_LIST < <(bw list items --pretty --session "$BW_SESSION")
+
 if [ "${#BW_LIST[@]}" -eq 0 ]; then
   notify-send -i 'status/security-high-symbolic' -u critical -t 3000 \
     'Vault retrieval failed' \
@@ -40,25 +45,43 @@ if [ "${#BW_LIST[@]}" -eq 0 ]; then
   exit 1
 fi
 
+if [ "$1" = "cards" ]; then
+  FILTER_COMMAND='.[]|select(.card)|{name, brand:.card.brand, favorite, notes, revisionDate, id, card}'
+else
+  FILTER_COMMAND='.[]|{name, user:.login.username, favorite, notes, password_updated:.login.passwordRevisionDate, last_updated:.login.revisionDate, id, card}'
+fi
+
 TARGET_ROW="$(echo "${BW_LIST[@]}" |
-  jq -C '.[]|{name, user:.login.username, favorite, notes, password_updated:.login.passwordRevisionDate, last_updated:.login.revisionDate, id}' -r --compact-output |
+  jq -C "$FILTER_COMMAND" -r --compact-output |
   fzf --height 12 -d ',' --nth 1,2,3,4 --with-nth 1,2 --tac --ansi --preview 'echo {} | jq -C')"
 if test -n "$TARGET_ROW"; then
   TARGET_ID="$(echo "$TARGET_ROW" | jq -r ".id")"
   TARGET_USER="$(echo "$TARGET_ROW" | jq -r ".user")"
   TARGET_PASSWORD="$(echo "${BW_LIST[@]}" | jq ".[]|select(.id == \"$TARGET_ID\")|.login.password" -r)"
+  TARGET_CARD_BRAND="$(echo "$TARGET_ROW" | jq -r ".card.brand")"
+  TARGET_CARD_NUMBER="$(echo "$TARGET_ROW" | jq -r ".card.number")"
+  TARGET_CARD_EXP_MONTH="$(echo "$TARGET_ROW" | jq -r ".card.expMonth")"
+  TARGET_CARD_EXP_YEAR="$(echo "$TARGET_ROW" | jq -r ".card.expYear")"
+  TARGET_CARD_CODE="$(echo "$TARGET_ROW" | jq -r ".card.code")"
 else
   exit 1
 fi
 
-if test -n "$TARGET_PASSWORD"; then
+if test -n "$TARGET_CARD_NUMBER"; then
+  echo -n "$TARGET_CARD_NUMBER" | xsel --primary
+  echo -n "$TARGET_CARD_CODE" | xsel --secondary
+  notify-send -i 'status/security-high-symbolic' -u normal -t 30000 \
+    "$TARGET_CARD_BRAND copied" \
+    "Exp. $TARGET_CARD_EXP_MONTH/$TARGET_CARD_EXP_YEAR ($TARGET_CARD_CODE)"
+  exit 0
+elif test -n "$TARGET_PASSWORD"; then
   if test -n "$TARGET_USER"; then
     echo -n "$TARGET_USER" | xsel --primary
   fi
   echo -n "$TARGET_PASSWORD" | xsel --secondary
   notify-send -i 'status/security-high-symbolic' -u low -t 5000 \
-    "Password copied to secondary clipboard" \
-    "$(echo "$TARGET_ROW" | jq -C '.user + " at " + .name' -r), valid for 30 seconds"
+    "User/password copied to primary/secondary clipboard" \
+    "$(echo "$TARGET_ROW" | jq -C '.user + " at " + .name' -r), valid for 60 seconds"
   exit 0
 else
   notify-send -i 'status/security-high-symbolic' -u critical -t 3000 \
