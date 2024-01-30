aed3046457ba594b89a36bf345ed422fdad6ade2
Evan Chen <evan@evanchen.cc>
2024-01-30 14:17:05 -0500

    feat(getclip): capture clipboard

--- sh-scripts/getclip.sh
+++ sh-scripts/getclip.sh
@@ -9,6 +9,8 @@ if [[ $contents == "qute://pdfjs/web/viewer.html?filename="* ]]; then
     cut -d "=" -f 2- |
     python -c "import urllib.parse, sys; print(urllib.parse.unquote(sys.stdin.readline()))")
 fi
-echo "$contents" | xsel --clipboard
+
+mkdir -p /tmp/clipboard/
+echo "$contents" | tee "/tmp/clipboard/$(date --iso-8601=seconds)" | xsel --clipboard
 
 notify-send -u low -i "edit-paste-symbolic" "xsel --clipboard" "${contents:0:160}"
