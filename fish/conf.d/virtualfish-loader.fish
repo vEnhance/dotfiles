e6de61c7a22440e403a043f7cb52448a8796239b
Evan Chen <evan@evanchen.cc>
2024-04-28 15:14:17 -0400

    feat(fish): python venv auto activation

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,5 @@
 set -g VIRTUALFISH_VERSION 2.5.8
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
 source /usr/lib/python3.12/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.12/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
