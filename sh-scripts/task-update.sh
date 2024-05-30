58067b261e8821e9d3ee3d96d1f030f565441d72
Evan Chen <evan@evanchen.cc>
2024-05-30 14:20:06 -0400

    edit: comment out bugwarrior for now
    Broken on Python 3.12

--- sh-scripts/task-update.sh
+++ sh-scripts/task-update.sh
@@ -5,9 +5,9 @@ set -euxo pipefail
 # Runs some routine taskwarrior work
 task sync
 
-if command -v bugwarrior-pull >/dev/null; then
-  bugwarrior-pull
-fi
+#if command -v bugwarrior-pull >/dev/null; then
+#  bugwarrior-pull
+#fi
 
 task rc.recurrence.limit=1 list
 
