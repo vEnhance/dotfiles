58fea8f1e35fb4d59af1b2c82b161fa74bd6137c
Evan Chen <evan@evanchen.cc>
2024-08-29 02:07:14 -0400

    fix(vim): explicit python host program for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.g.python3_host_prog = "/usr/bin/python3"
+vim.g.python_host_prog = "/usr/bin/python"
+
 vim.cmd 'set runtimepath^=~/.vim runtimepath+=~/.vim/after'
 vim.cmd 'let &packpath = &runtimepath'
 vim.cmd 'source ~/.vimrc'
