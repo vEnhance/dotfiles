a65fe395c3e6d4d48500842a7b6f29577d012a9e
Evan Chen <evan@evanchen.cc>
2024-09-03 18:15:02 -0400

    edit(conky): show 9 calendar events

--- conky/print-conky-today.sh
+++ conky/print-conky-today.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 if [ "$(whoami)" != "evan" ]; then
   exit
 fi
-color_num=(9 8 4 5 6)
+color_num=(9 8 4 5 6 6 6 6 6)
 
-for k in {1..5}; do
+for k in {1..9}; do
   if [ "$(wc --lines <~/.cache/agenda_today.txt)" -ge "$k" ]; then
     echo "\${color${color_num[k - 1]}}$(tail ~/.cache/agenda_today.txt -n +"${k}" | head -n 1)"
   fi
