41dc7d3a29454a76a104f105692b2127bb9a70e8
Evan Chen <evan@evanchen.cc>
2024-12-24 09:02:04 -0800

    feat(vim): play with colorscheme a bit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -15,17 +15,30 @@ return {
       transparent_background = true,
       custom_highlights = function(colors)
         return {
-          LineNr = { fg = colors.subtext0 },
+          LineNr = { fg = colors.peach },
           CursorLineNr = { fg = colors.teal, bg = colors.overlay0, style = { "bold" } },
+          Conceal = { fg = colors.yellow },
+          Identifier = { style = { "bold" } },
+          Include = { fg = colors.green },
+          Function = { fg = colors.red },
+          PreProc = { fg = colors.maroon, style = { "bold" } },
+          Special = { fg = colors.green, style = { "bold" } },
+          Statement = { fg = colors.sapphire, style = { "bold" } },
+          texTitleArg = { fg = colors.base, style = { "bold" } },
+          texEnvArgName = { fg = colors.red, style = { "bold" } },
+          texEnvOpt = { fg = colors.text, style = { "bold" } },
+          texMathEnvArgName = { fg = colors.green, style = { "bold" } },
+          texCmd = { fg = colors.blue, style = { "bold" } },
+          texCmdPart = { fg = colors.red, style = { "bold" } },
+          texPartArgTitle = { fg = colors.yellow, style = { "bold" } },
         }
       end,
     },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
