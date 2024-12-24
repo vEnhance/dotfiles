a2eb6b41a1c96ef22bf70261eed9a73c4859d6c9
Evan Chen <evan@evanchen.cc>
2024-12-23 18:58:08 -0800

    feat(nvim): start on plugins

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
