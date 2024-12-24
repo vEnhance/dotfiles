b12ee6c4d4b5340d7c20ee39302e6ae7e246b9d1
Evan Chen <evan@evanchen.cc>
2024-12-23 20:04:04 -0800

    feat(vim): add bdelete keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,3 +9,22 @@ vim.keymap.set("n", "<CR>", function()
     return "<CR>"
   end
 end, { expr = true, noremap = true })
+
+vim.keymap.set("n", "<Space>-", function()
+  vim.cmd([[
+    if (winnr('$') == 1 || (winnr('$') == 2 && winnr() == 2))
+      if tabpagenr('$') == 1
+        bdelete
+        if expand('%:p') ==# ''
+          quit
+        endif
+      else
+        bdelete
+      endif
+    elseif expand('%:l') ==# '__doc__'
+      bdelete
+    else
+      close
+    endif
+  ]])
+end, { noremap = true })
