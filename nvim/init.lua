bc73c6a0173ec0d6f4612aefb26b7cadb454664c
Evan Chen <evan@evanchen.cc>
2024-12-29 16:01:54 -1000

    edit(vim): vim tweaking

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,11 +8,6 @@ vim.cmd 'set runtimepath^=~/.vim runtimepath+=~/.vim/after'
 vim.cmd 'let &packpath = &runtimepath'
 vim.cmd 'source ~/.vimrc'
 
--- https://github.com/nvim-treesitter/nvim-treesitter/issues/5536#issuecomment-1826400562
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-}
-
 vim.diagnostic.config({
   virtual_text = {
     prefix = ' ●', -- Could be '●', '▎', 'x'
