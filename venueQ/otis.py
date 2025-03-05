85ff19fd78ef017777fbcb8a7d6ec112d03f3ea8
Evan Chen <evan@evanchen.cc>
2025-03-04 21:14:00 -0500

    fix(otis): use gvim rather than nvim-qt

--- venueQ/otis.py
+++ venueQ/otis.py
@@ -331,7 +331,7 @@ class ProblemSet(VenueQNode):
         if self.ext == "pdf":
             tool = "zathura"
         elif self.ext == "tex" or self.ext == "txt":
-            tool = "nvim-qt"
+            tool = "gvim"
         elif self.ext == "png" or self.ext == "jpg":
             tool = "feh"
         else:
