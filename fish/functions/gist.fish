d6dded114e734fcc50c9b94e8134c9c80cd1758c
Evan Chen <evan@evanchen.cc>
2025-04-03 17:54:52 -0400

    feat(fish): gist.fish command

---
+++ fish/functions/gist.fish
@@ -0,0 +1,25 @@
+function gist
+    set -l filename $argv[1]
+    set -l desc (string join " " $argv[2..-1])
+
+    if test -z "$filename"
+        echo "Usage: gist <filename> [description]"
+        return 1
+    end
+
+    # Create empty gist and extract URL
+    set -l url (echo -e "Type your content here." | gh gist create --filename "$filename" --desc "$desc" | string trim)
+
+    # Extract Gist ID from URL
+    set -l gist_id (basename $url)
+
+    # Clone it
+    gh gist clone $gist_id
+
+    # Rename directory
+    set -l base (path basename -E $filename)
+    mv $gist_id $base
+    echo "Cloned to: $base"
+
+    xdg-open $url
+end
