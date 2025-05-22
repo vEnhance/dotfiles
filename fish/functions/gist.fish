f51535a8ef07238a0c384e7d48e34803cb0793ee
Evan Chen <evan@evanchen.cc>
2025-05-22 12:54:35 -0400

    feat(gist): default extension of .md
    it feels more natural to type "gist example" than "gist example.md"

--- fish/functions/gist.fish
+++ fish/functions/gist.fish
@@ -1,23 +1,28 @@
 function gist
-    set -l filename $argv[1]
-    set -l desc (string join " " $argv[2..-1])
+    if string match --regex --quiet '\.[^./]+$' -- $argv[1]
+        set --function filename $argv[1]
+        set --function base (path basename -E $filename)
+    else
+        set --function base $argv[1]
+        set --function filename $argv[1].md
+    end
+    set --function desc (string join " " $argv[2..-1])
 
     if test -z "$filename"
-        echo "Usage: gist <filename> [description]"
+        echo "Usage: gist <basename> [description]"
         return 1
     end
 
     # Create empty gist and extract URL
-    set -l url (echo -e "Type your content here." | gh gist create --filename "$filename" --desc "$desc" | string trim)
+    set --function url (echo -e "Type your content here." | gh gist create --filename "$filename" --desc "$desc" | string trim)
 
     # Extract Gist ID from URL
-    set -l gist_id (basename $url)
+    set --function gist_id (basename $url)
 
     # Clone it
     gh gist clone $gist_id
 
     # Rename directory
-    set -l base (path basename -E $filename)
     mv $gist_id $base
     echo "Cloned to: $base"
 
