12cd287e7aa56cee81db6b48c0701871ec317f5e
Evan Chen <evan@evanchen.cc>
2025-07-30 09:41:26 -0400

    feat(fish): jail command

---
+++ fish/functions/jail.fish
@@ -0,0 +1,8 @@
+function jail -d "Starts a ethereal Fish shell"
+    fish --private --no-config --init-command=(string join "; " \
+        "fish_config prompt choose arrow" \
+        "fish_config theme choose 'Base16 Default Dark'" \
+        "source /usr/share/fish/functions/fish_greeting.fish" \
+        "fish_greeting" \
+    )
+end
