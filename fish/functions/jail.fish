bfe6e233eb9ab007817a151e817b7579c40c5878
Evan Chen <evan@evanchen.cc>
2025-08-01 08:38:48 -0400

    polish(fish): vi key bindings for ethereal

--- fish/functions/jail.fish
+++ fish/functions/jail.fish
@@ -4,5 +4,6 @@ function jail -d "Starts a ethereal Fish shell"
         "fish_config theme choose 'Base16 Default Dark'" \
         "source /usr/share/fish/functions/fish_greeting.fish" \
         "fish_greeting" \
+        "fish_vi_key_bindings" \
     )
 end
