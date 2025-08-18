536eafb82248e14115e02dde57b6f962f3031f23
Evan Chen <evan@evanchen.cc>
2025-08-18 10:48:50 -0400

    feat(yap): yap prompt to git push

--- fish/functions/yap.fish
+++ fish/functions/yap.fish
@@ -2,5 +2,12 @@ function yap --description "Scribble on the wall"
     cd ~/Sync/Projects/wall.evanchen.cc
     if python new_post.py
         git show
+        read -P "Push to remote? (y/n): " -l response
+        if test "$response" = y -o "$response" = yes
+            git push
+            ~/dotfiles/sh-scripts/noisemaker.sh 3
+        else
+            ~/dotfiles/sh-scripts/noisemaker.sh 4
+        end
     end
 end
