15402ff275e3f0b598e6762b4dc87a8d7d301bcc
Evan Chen <evan@evanchen.cc>
2025-08-19 10:12:50 -0400

    feat(conky): star bar for ArchDiamond conky

--- conky/run-conky.sh
+++ conky/run-conky.sh
@@ -17,6 +17,9 @@ if [ "$(hostname)" = ArchDiamond ]; then
     conky -d -c ~/dotfiles/conky/stats-power-widget.conf --xinerama-head=3
     conky -d -c ~/dotfiles/conky/cal.conf
   fi
+  if [ "$(whoami)" = "star" ]; then
+    conky -d -c ~/dotfiles/conky/star-bar.conf --xinerama-head=0
+  fi
 fi
 
 # Conky setup for main work desktops
