3fe5df3eb93b3f31b8b172ae2f53a956f84064dd
Evan Chen <evan@evanchen.cc>
2025-08-19 08:04:15 -0400

    fix(fish): make yap actually work

--- fish/functions/yap.fish
+++ fish/functions/yap.fish
@@ -1,13 +1,18 @@
-function yap --description "Scribble on the wall"
+function yap --description "Scribble on the wall at wall.evanchen.cc"
     cd ~/Sync/Projects/wall.evanchen.cc
+    vf activate wall
     if python new_post.py
         git show
         read -P "Push to remote? (y/n): " -l response
         if test "$response" = y -o "$response" = yes
-            git push
-            ~/dotfiles/sh-scripts/noisemaker.sh 3
+            if git push
+                nohup /home/evan/dotfiles/sh-scripts/noisemaker.sh 3 >/dev/null
+            else
+                nohup /home/evan/dotfiles/sh-scripts/noisemaker.sh 7 >/dev/null
+                sleep 10
+            end
         else
-            ~/dotfiles/sh-scripts/noisemaker.sh 4
+            nohup /home/evan/dotfiles/sh-scripts/noisemaker.sh 4 >/dev/null
         end
     end
 end
