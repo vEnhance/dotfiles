b3d469761c3d30f246db482813226a7902b9931a
Evan Chen <evan@evanchen.cc>
2025-08-26 16:00:35 -0400

    edit(fish): more triumphant yapping sound

--- fish/functions/yap.fish
+++ fish/functions/yap.fish
@@ -6,7 +6,7 @@ function yap --description "Scribble on the wall at wall.evanchen.cc"
         read -P "Push to remote? (y/n): " -l response
         if test "$response" = y -o "$response" = yes
             if git push
-                nohup /home/evan/dotfiles/sh-scripts/noisemaker.sh 3 >/dev/null
+                nohup /home/evan/dotfiles/sh-scripts/noisemaker.sh 2 >/dev/null
             else
                 nohup /home/evan/dotfiles/sh-scripts/noisemaker.sh 7 >/dev/null
                 sleep 10
