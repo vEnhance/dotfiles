108611df41b14224033dd3a288b975615de50612
Evan Chen <evan@evanchen.cc>
2025-09-14 12:38:21 -0700

    edit: xrandr monitor config ArchBootes

--- sh-scripts/xrandr.sh
+++ sh-scripts/xrandr.sh
@@ -35,10 +35,10 @@ fi
 
 if [ "$(hostname)" = ArchBootes ]; then
   xrandr \
-    --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal \
-    --output DP-0 --mode 2560x1440 --pos 6400x0 --rotate normal \
-    --output DP-4 --mode 2560x1440 --pos 8960x0 \
+    --output HDMI-0 --mode 1920x1080 --pos 0x2160 --rotate normal \
+    --output DP-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+    --output DP-0 --mode 2560x1440 --pos 3840x0 --rotate normal \
+    --output DP-4 --mode 2560x1440 --pos 7040x0 \
     ;
 fi
 
