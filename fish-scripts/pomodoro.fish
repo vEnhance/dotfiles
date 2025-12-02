62534bab6154c850238ca0ac72db9f36f04a7158
Evan Chen <evan@evanchen.cc>
2025-12-02 13:19:59 -0500

    edit: autostart pomodoro timer
    don't wait for an input

--- fish-scripts/pomodoro.fish
+++ fish-scripts/pomodoro.fish
@@ -3,7 +3,8 @@
 i3-msg '[class="^(?!Anki)"] move scratchpad'
 
 i3-msg workspace 8
-countdown 25m
+xterm -bg black -fg white -fa "Inconsolata Condensed ExtraBold" -fs 14 \
+    -e "termdown 25m -b -t END --exec-cmd \"if [ '{0}' == '1' ]; then sleep 1 && $HOME/dotfiles/sh-scripts/noisemaker.sh B; fi\"" &
 
 ~/dotfiles/sh-scripts/noisemaker.sh H
 touch "/tmp/pomodoro/$now.md"
