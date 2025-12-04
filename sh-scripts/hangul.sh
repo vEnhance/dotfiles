a65daf75d9ae4bd267d4e50b1c52c0bc1d8d8430
Evan Chen <evan@evanchen.cc>
2025-12-03 21:37:19 -0500

    feat(sh): hardcode hangul mode
    make it possible to type punctuation
    
    requires win+M to return to normal

--- sh-scripts/hangul.sh
+++ sh-scripts/hangul.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# if ibus is active, switch back to Eng/Dvorak
+# Dvorak w -> QWERTY comma
+xmodmap -e "keycode 59 = comma less comma less"
+# Dvorak v -> QWERTY period
+xmodmap -e "keycode 60 = period greater period greater"
+# Dvorak z -> QWERTY slash
+xmodmap -e "keycode 61 = slash question slash question"
+# Dvorak s -> QWERTY colon
+xmodmap -e "keycode 47 = semicolon colon semicolon colon"
+# Dvorak - -> QWERTY colon
+xmodmap -e "keycode 48 = apostrophe quotedbl apostrophe quotedbl"
+
 if pgrep -x ibus-daemon; then
   ibus engine "hangul"
 fi
