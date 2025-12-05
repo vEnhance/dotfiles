457584a5ab790939282b13b3f2079f1cee5b3004
Evan Chen <evan@evanchen.cc>
2025-12-05 14:22:12 -0500

    feat: read more calendars in get-cal.sh

--- sh-scripts/get-cal.sh
+++ sh-scripts/get-cal.sh
@@ -9,12 +9,13 @@ gcalendar --no-of-days 14 --output json \
   "Happiness" \
   "Important" \
   "Leisure" \
-  "Office Hours" \
   "Prison" \
   "Real Life" \
   "Schedule" \
   "Unfortunate Things" \
-  "Video Calls for OTIS" \
   "White" \
   "Zero-Minute Reminders" \
-  "twitch.tv" | python ~/dotfiles/py-scripts/get-cal-helper.py
+  "evan@evanchen.cc" \
+  "twitch.tv" \
+  "Accounts" \
+  "Contractor" | python ~/dotfiles/py-scripts/get-cal-helper.py
