70fbda2bdb897fe8587387ba58e6d3640b46c83e
Evan Chen <evan@evanchen.cc>
2025-12-20 18:13:55 -0800

    fix(sh): pacsnap vote explicit

--- sh-scripts/pacsnap.sh
+++ sh-scripts/pacsnap.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 pacman -Qqtten >~/Sync/pacman/"$(hostname)".pacman.paclist
 pacman -Qqttem >~/Sync/pacman/"$(hostname)".aur.paclist
 paclist chaotic-aur | cut -d " " -f 1 | sort | comm -23 - ~/Sync/pacman/excluded.txt >~/Sync/pacman/"$(hostname)".vote.paclist
-pacman -Qqm | sort | comm -23 - ~/Sync/pacman/excluded.txt >>~/Sync/pacman/"$(hostname)".vote.paclist
+pacman -Qqem | sort | comm -23 - ~/Sync/pacman/excluded.txt >>~/Sync/pacman/"$(hostname)".vote.paclist
 if [ "$(hostname)" = "$(cat ~/dotfiles/host-config/pacman)" ] && [ "$(whoami)" = "evan" ]; then
   cd ~/Sync/pacman/ || exit 1
   if ! git diff --exit-code; then
