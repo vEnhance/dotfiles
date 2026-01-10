8f24a0b17779a18a5e371744b43dc4cf9e8e3e17
Evan Chen <evan@evanchen.cc>
2026-01-10 09:35:15 -0800

    chore: bump virtualfish

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,5 +1,5 @@
 set -g VIRTUALFISH_VERSION 2.5.9
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /usr/lib/python3.13/site-packages/virtualfish/virtual.fish
-source /usr/lib/python3.13/site-packages/virtualfish/auto_activation.fish
+source /usr/lib/python3.14/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.14/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
