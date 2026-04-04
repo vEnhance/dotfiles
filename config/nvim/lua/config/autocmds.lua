be24a41360d734eaf80ab792b4c45946ac127614
Evan Chen <evan@evanchen.cc>
2026-04-04 13:23:34 -0400

    feat(nvim): ellipses in markdown

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.keymap.set("i", "...", "…", { buffer = true })
+  end,
+})
