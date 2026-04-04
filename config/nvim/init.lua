f72ca907fb1e6ec58bdf50996b5caccbb4677439
Evan Chen <evan@evanchen.cc>
2026-04-04 12:14:50 -0400

    feat(nvim): asy LSP i guess
    although it doesn't throw type errors so lowkey not that helpful

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,17 @@
 require("config.lazy")
+
+-- Asymptote LSP
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig.configs")
+
+if not configs.asy_ls then
+  configs.asy_ls = {
+    default_config = {
+      cmd = { "asy", "-lsp" },
+      filetypes = { "asy" },
+      single_file_support = true,
+      settings = {},
+    },
+  }
+end
+lspconfig.asy_ls.setup({})
