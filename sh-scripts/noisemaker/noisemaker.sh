1c0e218a6768b53e832f49c5f8d24c7a76f64014
Evan Chen <evan@evanchen.cc>
2026-04-30 08:39:43 -0400

    fix(sh): noisemaker should use -o pulse

--- sh-scripts/noisemaker/noisemaker.sh
+++ sh-scripts/noisemaker/noisemaker.sh
@@ -14,7 +14,7 @@ fi
 
 if pgrep -U "$(whoami)" "$PLAYER" >/dev/null; then
   if [ "$1" = a ]; then
-    "$PLAYER" -q -f -18000 "$DIR"/Sa-horn.mp3 &
+    "$PLAYER" -o pulse -q -f -18000 "$DIR"/Sa-horn.mp3 &
     exit
   else
     pkill "$PLAYER"
@@ -22,45 +22,45 @@ if pgrep -U "$(whoami)" "$PLAYER" >/dev/null; then
 fi
 
 if [ "$1" = 1 ]; then
-  "$PLAYER" -q "$DIR"/S1-applause.mp3 &
+  "$PLAYER" -o pulse -q "$DIR"/S1-applause.mp3 &
 fi
 if [ "$1" = 2 ]; then
-  "$PLAYER" -q -f -9000 "$DIR"/S2-success.mp3 &
+  "$PLAYER" -o pulse -q -f -9000 "$DIR"/S2-success.mp3 &
 fi
 if [ "$1" = 3 ]; then
-  "$PLAYER" -q -f 15000 "$DIR"/S3-fanfare.mp3 &
+  "$PLAYER" -o pulse -q -f 15000 "$DIR"/S3-fanfare.mp3 &
 fi
 if [ "$1" = 4 ]; then
-  "$PLAYER" -q "$DIR"/S4-correct.mp3 &
+  "$PLAYER" -o pulse -q "$DIR"/S4-correct.mp3 &
 fi
 if [ "$1" = 5 ]; then
-  "$PLAYER" -q "$DIR"/S5-cashreg.mp3 &
+  "$PLAYER" -o pulse -q "$DIR"/S5-cashreg.mp3 &
 fi
 if [ "$1" = 6 ]; then
-  "$PLAYER" -q -f -13000 "$DIR"/S6-metal.mp3 &
+  "$PLAYER" -o pulse -q -f -13000 "$DIR"/S6-metal.mp3 &
 fi
 if [ "$1" = 7 ]; then
-  "$PLAYER" -q "$DIR"/S7-drum.mp3 &
+  "$PLAYER" -o pulse -q "$DIR"/S7-drum.mp3 &
 fi
 if [ "$1" = 8 ]; then
-  "$PLAYER" -q "$DIR"/S8-boo.mp3 &
+  "$PLAYER" -o pulse -q "$DIR"/S8-boo.mp3 &
 fi
 if [ "$1" = 9 ]; then
-  "$PLAYER" -q -f -18000 "$DIR"/S9-taratata.mp3 &
+  "$PLAYER" -o pulse -q -f -18000 "$DIR"/S9-taratata.mp3 &
 fi
 if [ "$1" = 0 ]; then
-  "$PLAYER" -q "$DIR"/S0-success.mp3 &
+  "$PLAYER" -o pulse -q "$DIR"/S0-success.mp3 &
 fi
 
 if [ "$1" = a ]; then
-  "$PLAYER" -q -f -18000 "$DIR"/Sa-horn.mp3 &
+  "$PLAYER" -o pulse -q -f -18000 "$DIR"/Sa-horn.mp3 &
 fi
 if [ "$1" = B ]; then
-  "$PLAYER" -q -f -80000 "$DIR"/SB-buzz.mp3 &
+  "$PLAYER" -o pulse -q -f -80000 "$DIR"/SB-buzz.mp3 &
 fi
 if [ "$1" = N ]; then
-  "$PLAYER" -q -f -80000 "$DIR"/SN-nooo.mp3 &
+  "$PLAYER" -o pulse -q -f -80000 "$DIR"/SN-nooo.mp3 &
 fi
 if [ "$1" = H ]; then
-  "$PLAYER" -q -f -80000 "$DIR"/SH-holy.mp3 &
+  "$PLAYER" -o pulse -q -f -80000 "$DIR"/SH-holy.mp3 &
 fi
