ee3522feb036db3121de1c6fefe3100a075c5f75
Evan Chen <evan@evanchen.cc>
2026-05-01 16:00:21 -0400

    fix: simplify pikaur wrapper

--- config/fish/functions/pikaur.fish
+++ config/fish/functions/pikaur.fish
@@ -1,14 +1,6 @@
-# pikaur hack to automatically disable virtualenvs
 function pikaur
     if set -q VIRTUAL_ENV
-        set existing_venv (basename $VIRTUAL_ENV)
-        echo "Temporarily disabling" (set_color --bold brcyan)"$existing_venv"
-        vf deactivate
-        set_color normal
-        /usr/bin/pikaur $argv
-        echo Re-enabling (set_color --bold brcyan)"$existing_venv"
-        vf activate "$existing_venv"
-        set_color normal
+        env -u VIRTUAL_ENV PATH=(string join : (string match -v "$VIRTUAL_ENV/bin" $PATH)) /usr/bin/pikaur $argv
     else
         /usr/bin/pikaur $argv
     end
