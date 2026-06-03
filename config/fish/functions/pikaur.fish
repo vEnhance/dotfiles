b6adde7839c62666330ca5b7ade2872324b7cd1b
Evan Chen <evan@evanchen.cc>
2026-06-03 10:40:36 -0700

    drop: pikaur hack
    it was always kind of icky tbh

--- config/fish/functions/pikaur.fish
+++
@@ -1,7 +0,0 @@
-function pikaur
-    if set -q VIRTUAL_ENV
-        env -u VIRTUAL_ENV PATH=(string join : (string match -v "$VIRTUAL_ENV/bin" $PATH)) /usr/bin/pikaur $argv
-    else
-        /usr/bin/pikaur $argv
-    end
-end
