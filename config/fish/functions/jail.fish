c88189e232859851c8b1d327c0b47cbb7e425f2c
Evan Chen <evan@evanchen.cc>
2026-07-16 01:38:21 -0700

    fix(fish): no fish_greeting.fish in /usr/share

--- config/fish/functions/jail.fish
+++ config/fish/functions/jail.fish
@@ -2,7 +2,6 @@ function jail -d "Starts a ethereal Fish shell"
     fish --private --no-config --init-command=(string join "; " \
         "fish_config prompt choose arrow" \
         "fish_config theme choose 'Base16 Default Dark'" \
-        "source /usr/share/fish/functions/fish_greeting.fish" \
         "fish_greeting" \
         "fish_vi_key_bindings" \
     )
