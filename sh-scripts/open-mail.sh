b728f5bac38f96bd1ea75a6d9f1237103bdb5002
Evan Chen <evan@evanchen.cc>
2026-07-23 19:39:31 -0700

    feat: don't run neomutt in venv
    breaks system scripts

--- sh-scripts/open-mail.sh
+++ sh-scripts/open-mail.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+if [[ -n ${VIRTUAL_ENV:-} ]]; then
+  echo "Refusing to run inside a Python virtual environment" >&2
+  echo "(VIRTUAL_ENV=$VIRTUAL_ENV)" >&2
+  exit 1
+fi
+
 set -euxo pipefail
 
 mbsync -q personal-inbox work-inbox records-inbox &
