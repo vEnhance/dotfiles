5876cfdb8e4e0d3237955e654cbf9fde1d2d2a65
Evan Chen <evan@evanchen.cc>
2026-09-01 23:01:21 -0400

    style(sh): minor improvements to cron.sh

--- sh-scripts/cron.sh
+++ sh-scripts/cron.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
+set -u
+
 # Check if we have Internet connection
 # as this command is kind of pointless without it
-
 if ! wget -q --spider https://web.evanchen.cc; then
   echo "No Internet"
   exit 0
 fi
+
 if PASSWORD_STORE_GPG_OPTS="--pinentry-mode error" pass show hello >/dev/null 2>&1; then
   PASS_UNLOCKED=1
 else
   PASS_UNLOCKED=0
 fi
+echo "PASS_UNLOCKED = $PASS_UNLOCKED"
 
 # This command grabs all the OTIS stuff: problem sets, petitions, suggestions
 # and processes all of them through venueQ
-if [ "$(hostname)" = "$(jq --raw-output .otis ~/secrets/host-config.json)" ] && [ "$(whoami)" = "evan" ] && [ "$PASS_UNLOCKED" = 1 ]; then
+if [ "$(hostname)" = "$(jq --raw-output .otis ~/secrets/host-config.json)" ] && [ "$PASS_UNLOCKED" = 1 ]; then
   /usr/bin/python3 ~/dotfiles/py-scripts/venueQ/otis.py
 fi
 
@@ -23,7 +26,7 @@ fi
 # It's a program that'll read the next 14 days of my calendar
 # and output the results under ~/.cache/agenda.json
 # where it can be consumed by e.g. conky
-if command -v gcalendar; then
+if command -v gcalendar >/dev/null; then
   ~/dotfiles/sh-scripts/get-cal.sh
 fi
 
@@ -31,20 +34,17 @@ fi
 if [ "$(whoami)" = "evan" ]; then
   task sync
   if [ "$(hostname)" = "$(jq --raw-output .task ~/secrets/host-config.json)" ]; then
-    #if command -v bugwarrior-pull >/dev/null; then
-    #  bugwarrior-pull
-    #fi
-    task rc.recurrence.limit=1 list
+    task rc.recurrence.limit=1 list >/dev/null
     task sync
   fi
 fi
 
-if [ -f /bin/pacman ] && [ -d ~/Sync/pacman ]; then
+if command -v pacman >/dev/null && [ -d ~/Sync/pacman ]; then
   ~/dotfiles/sh-scripts/pacsnap.sh
 fi
 
 ## MBSYNC + MUTT
 # Syncing mailboxes for use with mutt
-if command -v mbsync && command -v pass && [ "$PASS_UNLOCKED" = 1 ]; then
+if command -v mbsync >/dev/null && [ "$PASS_UNLOCKED" = 1 ]; then
   mbsync -q personal-inbox work-inbox records-inbox
 fi
