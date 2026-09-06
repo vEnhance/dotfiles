1fcd6b581d1f32ffc2b45990a734370aeacef6cc
Evan Chen <evan@evanchen.cc>
2026-09-06 14:36:52 -0400

    fix: pomodoro touch in wrong place

--- config/fish/functions/pomodoro.fish
+++ config/fish/functions/pomodoro.fish
@@ -6,9 +6,9 @@ function pomodoro
         -e "termdown 25m -b -t END --exec-cmd \"if [ '{0}' == '1' ]; then sleep 1 && $HOME/dotfiles/sh-scripts/noisemaker/noisemaker.sh B; fi\"" &
 
     ~/dotfiles/sh-scripts/noisemaker/noisemaker.sh H
-    touch "/tmp/pomodoro/$now.md"
     set now (date +%Y%m%d-%H%M%S)
     mkdir -p /tmp/pomodoro
+    touch "/tmp/pomodoro/$now.md"
 
     i3-msg split h
     xfce4-terminal -e "nvim /tmp/pomodoro/$now.md" &
