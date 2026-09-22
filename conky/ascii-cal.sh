1f76796a8399c4b218253e93a8f72f95fbe13e01
Evan Chen <evan@evanchen.cc>
2026-09-22 07:35:56 -0400

    feat(conky): font refactor
    make korean and ascii line up correctly

--- conky/ascii-cal.sh
+++ conky/ascii-cal.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+LANG=ko_KR.UTF-8
 TODAY=$(date +%e)
-cal | sed '1d' | sed 's/^/ /g' | sed 's/$/ /g' | sed "/ $TODAY /s/ $TODAY /\${font3}\${color ffffff} $TODAY \${font2}\${color bbbbbb}/"
+cal | sed '1d' | sed 's/^/ /g' | sed 's/$/ /g' | sed "/ $TODAY /s/ $TODAY /\${font5}\${color ffffff} $TODAY \${font4}\${color bbbbbb}/"
